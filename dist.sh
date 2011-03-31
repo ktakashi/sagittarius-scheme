1de8fab7d1bc7767bdc21b7dd92ca1837e25c559
t.kato <tk_riple@yahoo.co.jp>
2011-03-31 11:06:48 +0200

    Added some files

---
+++ dist.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# TODO may be get version number somewhere
+export DIST_NAME=sagittarius-0.0.1
+
+# just in case
+if [ -e ${DIST_NAME} ]; 
+then
+    rm -rf ${DIST_NAME}
+fi
+
+# copy to tmp directory
+mkdir ${DIST_NAME}
+cp -r boot/ ${DIST_NAME}
+cp -r src/ ${DIST_NAME}
+cp -r lib/ ${DIST_NAME}
+cp -r sitelib/ ${DIST_NAME}
+cp -r cmake/ ${DIST_NAME}
+cp -r script/ ${DIST_NAME}
+cp -r test/ ${DIST_NAME}
+cp -r unicode/ ${DIST_NAME}
+cp -r autogen.sh ${DIST_NAME}
+cp -r geninsn.sh ${DIST_NAME}
+cp -r CMakeLists.txt ${DIST_NAME}
+cp -r README ${DIST_NAME}
+cp -r Copyright ${DIST_NAME}
+
+tar -czvf ${DIST_NAME}.tar.gz ${DIST_NAME}
+
+# clean up
+rm -rf ${DIST_NAME}
