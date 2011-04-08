8e501e3cacb5f27a163100242a50d08c7171768f
t.kato <tk_riple@yahoo.co.jp>
2011-04-08 16:56:45 +0200

    repl.scm: added for convenience

---
+++ repl.scm
@@ -0,0 +1,41 @@
+(import (rnrs)
+	(core errors))
+;; dummy
+(define-syntax environment
+  (syntax-rules ()
+    ((_ _)
+     '())))
+
+(define (default-reader)
+  (read (current-input-port)))
+
+(define (default-evaluator exp env)
+  (eval exp env))
+
+(define (default-printer . args)
+  (for-each (lambda (arg)
+	      (write/ss arg)(newline))
+	    args))
+
+(define (default-prompter)
+  (display "sash> ")
+  (flush-output-port (current-output-port)))
+
+(define (repl reader evaluator printer prompter)
+  (let loop1 ()
+    (and
+     (with-exception-handler
+       (lambda (e) (display (describe-condition e)) #t)
+       (lambda ()
+	 (prompter)
+         (let loop2 ((exp (reader)))
+           (if (eof-object? exp)
+               #f
+               (begin
+                 (receive result (evaluator exp (environment '(rnrs)))
+                   (apply printer result)
+		   (prompter))
+                 (loop2 (reader)))))))
+     (loop1))))
+
+(repl default-reader default-evaluator default-printer default-prompter)
