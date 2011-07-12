d1c7df957128b4f211481a9ddaf098ea13f08207
t.kato <tk_riple@yahoo.co.jp>
2011-07-12 10:17:55 +0200

    removed deprecated files.

--- sitelib/sagittarius/define-optional.scm
+++
@@ -1,5 +0,0 @@
-;; -*- scheme -*-
-#!deprecated
-(library (sagittarius define-optional)
-    (export define-optional)
-    (import (sagittarius control)))
--- sitelib/sagittarius/let-optionals%2a.scm
+++
@@ -1,6 +0,0 @@
-;; -*- scheme -*-
-#!deprecated
-(library (sagittarius let-optionals*)
-    (export let-optionals*
-	    get-optional)
-    (import (sagittarius control)))
