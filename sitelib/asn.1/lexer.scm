6486f3eb4dcd3e41c2bbd83195f477efeccc7267
t.kato <tk_riple@yahoo.co.jp>
2011-08-16 19:20:16 +0200

    rewrote asn.1 lexer.

--- sitelib/asn.1/lexer.scm
+++ sitelib/asn.1/lexer.scm
@@ -9,6 +9,7 @@
 	    (asn.1 types)
 	    (text parse)
 	    (srfi :1 lists)
+	    (srfi :2 and-let*)
 	    (srfi :13 strings)
 	    (srfi :14 char-set)
 	    (sagittarius)
@@ -42,10 +43,12 @@
       ("OPTIONAL"   . OPTIONAL)
       ("SEQUENCE"   . SEQUENCE)
       ("SET"        . SET)
-      (","          . COMMA)
-      ("{"          . LBRACE)
-      ("}"          . RBRACE)
-      ("::="        . ASSIGN)))
+      #;("::="        . ASSIGN)))
+
+  (define reserved-char
+    '((#\, . COMMA)
+      (#\{ . LBRACE)
+      (#\} . RBRACE)))
 
   (define class-tags 
     `(("APPLICATION" . ,CLASS_APPLICATION)
@@ -54,82 +57,104 @@
       ("CONTEXT"     . ,CLASS_CONTEXT)))
 
   (define word-charset (string->char-set "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-"))
+  (define class-charset (string->char-set "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"))
 
   (define (make-lexer inport)
     (lambda ()
       (define stacked '())
-      (letrec ((skip-space (lambda () (skip-while char-set:whitespace inport)))
-	       (next-token (lambda () (next-token-of char-set:graphic inport))))
-	(skip-space)
+      (define prev #f)
+      (letrec* ((skip-space (lambda () (skip-while char-set:whitespace inport)))
+		(next-char  (lambda () (skip-space) (get-char inport)))
+		(peek-char  (lambda () (skip-space) (lookahead-char inport)))
+		(next-token (lambda (set) (skip-space) (next-token-of set inport))))
 	(let loop ()
-	  (if (null? stacked)
-	      (let ((location (apply make-source-location (append! (port-info inport) '(-1 -1))))
-		    (token (next-token)))
-		(cond ((string-null? token) '*eoi*)
-		      ((and (> (string-length token) 2) (string= token "--" 0 2))
-		       ;; comment
-		       (loop))
-		      ((assoc token reserved string=?)
-		       => (lambda (slot)
-			    ;; a comma is not required after a '}' so to aid the
-			    ;; parser we insert a fake token after any '}'
-			    (if (string=? token "}")
-				(append! stacked '(POSTRBRACE)))
-			    (make-lexical-token (cdr slot) location token)))
-		      ((and (or (string=? token "OCTET")
-				(string=? token "BIT"))
-			    (let ((next (next-token)))
-			      (if (string=? next "STRING")
-				  next
-				  #f)))
-		       => (lambda (after)
-			    (make-lexical-token 'WORD location (string->symbol
-								(format "~a-~a" token after)))))
-		      ((and (string=? token "OBJECT")
-			    (let ((next (next-token)))
-			      (if (string=? next "IDENTIFIER")
-				  next
-				  #f)))
-		       => (lambda (after)
-			    (make-lexical-token 'WORD location (string->symbol
-								(format "~a-~a" token after)))))
-		      ((string=? token "RELATIVE-OID")
-		       (make-lexical-token 'WORD location 'RELATIVE-OID))
-		      ((string-every word-charset token)
-		       (make-lexical-token 'WORD location (string->symbol token)))
-		      ((and (char=? (string-ref token 0) #\[) ;; [APPLICATION|PRIVATE...]
-			    (let ((next (if (= (string-length token) 1)
-					    (next-token)
-					    (substring token 1 (string-length token)))))
-			      (cond ((assoc next class-tags string=)
-				     => (lambda (slot)
-					  (skip-space)
-					  (let* ((n (next-token))
-						 (with (char=? #\] (string-ref n (- (string-length n) 1))))
-						 (brace (if with
-							    (substring n (- (string-length n) 1) 1)
-							    (next-token))))
-					    (when with
-					      (set! n (substring n 0 (- (string-length n) 1))))
-					    (cond ((string->number n)
-						   => (lambda (number)
-							(make-lexical-token 'CLASS location (encode-tag (cdr slot) number))))
-						  (else #f)))))
-				    ((string->number next)
-				     => (lambda (number)
-					  (if (string=? (next-token) "]")
-					      (make-lexical-token 'CLASS location (encode-tag CLASS_CONTEXT number))
-					      #f)))
-				    (else #f)))))
-		      ((string->number token)
-		       (make-lexical-token 'NUMBER location token))
-		      (else
-		       (raise-asn.1-lexical-error 'asn.1-lexer
-						  (format "Parse error file ~a line ~a"
-							  (source-location-input location)
-							  (source-location-line location))
-						  token))))
-	      (let ((ret (car stacked)))
-		(set! stacked (cdr stacked))
-		(make-lexical-token ret #f #f)))))))
+	  (cond (prev 
+		 ;; previous token
+		 (let ((p prev))
+		   (set! prev #f)
+		   p))
+		((null? stacked)
+		 (let ((location (apply make-source-location (append! (port-info inport) '(-1 -1))))
+		       (ch (peek-char)))
+		   (cond ((eof-object? ch) '*eoi*)
+			 ((and (char=? ch #\-)
+			       (char=? (peek-next-char inport) #\-))
+			  ;; comment
+			  ;; white space must be skipped by now, so just discard a line
+			  (get-line inport)(loop))
+			 ((or (char=? ch #\,)
+			      (char=? ch #\{) (char=? ch #\}))
+			  (when (char=? ch #\})
+			    (append! stacked (list 'POSTBRACE)))
+			  (let ((slot (assv ch reserved-char)))
+			    (make-lexical-token (cdr slot) location (list->string (list ch)))))
+			 ;; we treat assign special way.
+			 ((and (char=? ch #\:)
+			       (char=? (peek-next-char inport) #\:)
+			       (char=? (peek-next-char inport) #\=))
+			  (get-char inport) ;; discard #\=
+			  (make-lexical-token 'ASSIGN location "::="))
+			 ((and (char-set-contains? word-charset ch)
+			       (next-token word-charset))
+			  => (lambda (token)
+			       ;; reserved word or variable
+			       (cond ((assoc token reserved)
+				      => (lambda (slot)
+					   (make-lexical-token (cdr slot) location token)))
+				     ((and (or (string=? token "OCTET")
+					       (string=? token "BIT"))
+					   (and-let* ((next (next-token word-charset))
+						      ( (set! prev next) )
+						      ( (string=? next "STRING") ))
+					     (make-lexical-token 'WORD location
+								 (string-append token "-" next)))))
+				     
+				     ((and (string=? token "OBJECT")
+					   (and-let* ((next (next-token word-charset))
+						      ( (set! prev next) )
+						      ( (string=? next "IDENTIFIER") ))
+					     (make-lexical-token 'WORD location
+								 (string-append token "-" next)))))
+				     ((string=? token "RELATIVE-OID")
+				      (make-lexical-token 'WORD location token))
+				     (else
+				      (make-lexical-token 'WORD location token)))))
+			 ((char=? ch #\[)
+			  ;; class
+			  ;; discard #\[
+			  (get-char inport)
+			  (let* ((token (next-token class-charset))
+				 (digit? (string->number token 10)))
+			    (unless (or (assoc token class-tags)
+					digit?)
+			      (raise-asn.1-lexical-error 'lexer "invalid class tag token" token))
+			    (if digit?
+				(let ((next (next-char)))
+				  ;; check #\]
+				  (unless (char=? next #\])
+				    (raise-asn.1-lexical-error 'lexer "no right bracket for class tag" token))
+				  (make-lexical-token 'CLASS location (encode-tag CLASS_CONTEXT digit?)))
+				(let ((tag  (assoc token class-tags))
+				      (next (next-token char-set:digit)))
+				  (unless tag
+				    (raise-asn.1-lexical-error 'lexer "invalid class tag" token))
+				  (when (string-null? next)
+				    (raise-asn.1-lexical-error 'lexer "invalid class tag" token))
+				  (let ((br (next-char)))
+				    ;; check #\]
+				    (unless (char=? br #\])
+				      (raise-asn.1-lexical-error 'lexer "no right bracket for class tag" token))
+				    (make-lexical-token 'CLASS location 
+							(encode-tag (cdr tag) (string->number next 10))))))))
+			 ((and-let* (( (char=? ch #\() )
+				     (token (next-token char-set:digit))
+				     ( (not (string-null? token)) )
+				     ( (char=? (peek-char) #\)) ))
+			    (make-lexical-token 'NUMBER token)))
+			 (else
+		       (raise-asn.1-lexical-error 'lexer "invalid token" ch)))))
+		(else 
+		 (let ((ret (car stacked)))
+		   (set! stacked (cdr stacked))
+		   (make-lexical-token ret #f #f))))))))
   )
