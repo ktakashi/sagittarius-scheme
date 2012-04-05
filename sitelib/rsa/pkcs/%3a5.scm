b55f697ffc14d2532453f21d5e6c16ded89a4abb
t.kato <devnull@localhost>
2012-04-05 13:31:49 +0200

    Added pkcs5 library.

---
+++ sitelib/rsa/pkcs/%3a5.scm
@@ -0,0 +1,233 @@
+;;; -*- Scheme -*-
+;;;
+;;; pkcs 5.scm - Password Based Cryptography library.
+;;;  
+;;;   Copyright (c) 2010-2012  Takashi Kato  <ktakashi@ymail.com>
+;;;   
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;   
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;  
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;  
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;  
+
+;; Key derivation and PBES1 are supported.
+;; PBES2 and MAC are not yet.
+(library (rsa pkcs :5)
+    (export pbkdf-1 pbkdf-2 derive-key
+	    PKCS5-S1
+	    pbe-with-md5-and-des pbe-with-sha1-and-des
+	    make-pbe-parameter generate-secret-key
+	    ;; To reuse cipher, we also need to export this method
+	    derive-key&iv
+	    <pbe-secret-key> <pbe-parameter>
+	    <pbe-cipher-spi>)
+    (import (rnrs) (clos user) (crypto)
+	    (rfc hmac)
+	    (util bytevector)
+	    (sagittarius)
+	    (math) (sagittarius control))
+
+  (define (pbkdf-2 P S c dk-len 
+		   :key (algo :hash (hash-algorithm SHA-1))
+		   (prf (hash-algorithm HMAC :key P :hash algo)))
+    (define (F P S c i)
+      (define (concat bv int)
+	(let* ((len (bytevector-length bv))
+	       (new (make-bytevector (+ len 4)))
+	       (iv  (make-bytevector 4)))
+	  (bytevector-u32-set! iv 0 int 'big)
+	  (bytevector-copy! bv 0 new 0 len)
+	  (bytevector-copy! iv 0 new len 4)
+	  new))
+      ;; create us
+      (let* ((size (hash-size prf))
+	     (buf (make-bytevector size))
+	     (out (make-bytevector size)))
+	(dotimes (j c)
+	  (cond ((zero? j)
+		 (hash! prf out (concat S i))
+		 (bytevector-copy! out 0 buf 0 size))
+		(else
+		 (hash! prf buf buf)
+		 (bytevector-xor! out out buf))))
+	out))
+    ;; (- (expt 2 32) 1) -> #xffffffff
+    (when (> dk-len (* #xffffffff (hash-size prf)))
+      (assertion-violation 'pbkdf-2
+			   "derived key too long"))
+    (let* ((h-len (hash-size prf))
+	   (l (ceiling (/ dk-len h-len)))
+	   (r (* (- dk-len (- l 1) h-len)))
+	   (ts (make-vector l))
+	   (unit-size (hash-size prf)))
+      (dotimes (i l)
+	(vector-set! ts i (F P S c (+ i 1))))
+      ;; concat
+      (let ((dk (make-bytevector dk-len)))
+	(let loop ((stored 0)
+		   (i 0))
+	  (unless (= stored dk-len)
+	    (let ((count (if (>= (- dk-len stored) unit-size)
+			     unit-size
+			     (- dk-len stored))))
+	      (bytevector-copy! (vector-ref ts i) 0 dk stored count)
+	      (loop (+ count stored) (+ i 1)))))
+	dk)))
+
+  (define (pbkdf-1 P S c dk-len :key (algo :hash (hash-algorithm SHA-1)))
+    (when (> dk-len (hash-size algo))
+      (assertion-violation 'pbkdf-1
+			   "derived key too long"))
+    (let* ((buf   (make-bytevector (hash-size algo)))
+	   (out   (make-bytevector dk-len)))
+      (hash-init! algo)
+      (hash-process! algo P)
+      (hash-process! algo S)
+      (hash-done! algo buf)
+      (dotimes (i (- c 1))
+	(hash! algo buf buf))
+      (bytevector-copy! buf 0 out 0 dk-len)
+      out))
+
+  (define (derive-key P S c dk-len :key (kdf pbkdf-2) :allow-other-keys rest)
+    (unless (and (bytevector? P) (bytevector? S))
+      (assertion-violation 'derive-key
+			   "bytevector required" P S))
+    (unless (and (integer? c) (positive? c))
+      (assertion-violation 'derive-key
+			   "positive integer required for iteration count") c)
+    (apply kdf P S c dk-len rest))
+
+  (define-class <pbe-secret-key> (<symmetric-key>)
+    ((password :init-keyword :password)
+     (hash     :init-keyword :hash)
+     (scheme   :init-keyword :scheme)
+     (type     :init-keyword :type)
+     (iv-size  :init-keyword :iv-size)
+     (length   :init-keyword :length)))
+
+  ;; markers
+  (define-class <pkcs5-s1> () ())
+  ;; PKCS#5 defines combination of MD5, SHA1 and DES, RC2.
+  ;; but we do not support RC2
+  ;; PBEWithMD5AndDES
+  ;; PBEWithSHA1AndDES
+  (define-class <pbe-md5-des> () ())
+  (define-class <pbe-sha1-des> () ())
+
+  (define PKCS5-S1 (make <pkcs5-s1>))
+  ;; the names are used also for cipher
+  (define pbe-with-md5-and-des (make <pbe-md5-des>))
+  (define pbe-with-sha1-and-des (make <pbe-sha1-des>))
+
+  ;; PBE parameter, it holds salt and iteration count.
+  (define-class <pbe-parameter> ()
+    ((salt      :init-keyword :salt)
+     (iteration :init-keyword :iteration)))
+  (define (make-pbe-parameter salt iteration)
+    (unless (bytevector? salt)
+      (assertion-violation 'make-pbe-parameter
+			   "salt must be a bytevector" salt))
+    (unless (and (integer? iteration)
+		 (positive? iteration))
+      (assertion-violation 'make-pbe-parameter
+			   "iteration must be a non negative exact integer"
+			   iteration))
+    (make <pbe-parameter> :salt salt :iteration iteration))
+
+  ;; secret key generation
+  ;; DES: key length 8, iv length 8
+  (define-method generate-secret-key ((marker <pbe-md5-des>)
+				      (password <string>))
+    (make <pbe-secret-key> :password password :hash (hash-algorithm MD5)
+	  :scheme DES :iv-size 8 :length 8
+	  :type PKCS5-S1))
+  (define-method generate-secret-key ((marker <pbe-sha1-des>)
+				      (password <string>))
+    (make <pbe-secret-key> :password password :hash (hash-algorithm SHA-1)
+	  :scheme DES :iv-size 8 :length 8
+	  :type PKCS5-S1))
+
+  ;; for pbe-cipher-spi we need derive derived key and iv from given
+  ;; secret key and parameter(salt and iteration count)
+  ;; see PKCS#5 or RFC2898 section 6.1.1 Encryption Operation
+  (define-method derive-key&iv ((marker <pkcs5-s1>)
+				(key <pbe-secret-key>)
+				(param <pbe-parameter>))
+    (define (derive-pkcs5-s1-key key parameter)
+      (define (separate-key-iv dk key-len iv-len)
+	(let ((k (make-bytevector key-len))
+	      (iv (make-bytevector iv-len)))
+	  (bytevector-copy! dk 0 k 0 key-len)
+	  (bytevector-copy! dk key-len iv 0 iv-len)
+	  (cons k iv)))
+      (let ((derived-key (derive-key (string->utf8 (slot-ref key 'password))
+				     (slot-ref parameter 'salt)
+				     (slot-ref parameter 'iteration)
+				     (hash-size (slot-ref key 'hash))
+				     :kdf pbkdf-1
+				     :hash (slot-ref key 'hash))))
+	(separate-key-iv derived-key
+			 (slot-ref key 'length) 
+			 (slot-ref key 'iv-size))))
+    (derive-pkcs5-s1-key key param))
+
+  ;; The PKCS#5 cipher. This can be used by PKCS#12 too.
+  (define-class <pbe-cipher-spi> (<cipher-spi>)
+    ((parameter :init-keyword :parameter)
+     (cipher    :init-keyword :cipher)))
+  (define-method initialize ((spi <pbe-cipher-spi>) initargs)
+    (let ((key (car initargs))
+	  (rest (cdr initargs)))
+      (unless (is-a? key <pbe-secret-key>)
+	(assertion-violation 'initialize
+			     "<pbe-secret-key> required" key))
+      (let-keywords* rest
+	  ((parameter #f))
+	(unless parameter
+	  (assertion-violation 
+	   'initialize
+	   "required keyword argument :parameter is missing"))
+	(unless (is-a? parameter <pbe-parameter>)
+	  (assertion-violation 'initialize
+			       "parameter must be instance ob <pbe-parameter>"
+			       parameter))
+	(let* ((key&iv (derive-key&iv (slot-ref key 'type) key parameter))
+	       (derived-secret-key (generate-secret-key (slot-ref key 'scheme)
+							(car key&iv)))
+	       (real-cipher 
+		(cipher (slot-ref key 'scheme) derived-secret-key
+			:mode MODE_CBC
+			:iv (cdr key&iv))))
+	  (slot-set! spi 'name (format "pbe-~a" (slot-ref key 'scheme)))
+	  (slot-set! spi 'key derived-secret-key)
+	  (slot-set! spi 'encrypt (lambda (pt key)
+				    (encrypt real-cipher pt)))
+	  (slot-set! spi 'decrypt (lambda (ct key)
+				    (decrypt real-cipher ct)))
+	  (slot-set! spi 'padder #f)
+	  (slot-set! spi 'signer #f)
+	  (slot-set! spi 'verifier #f)
+	  (slot-set! spi 'keysize (slot-ref key 'length))))))
+
+  (register-spi pbe-with-md5-and-des <pbe-cipher-spi>)
+  (register-spi pbe-with-sha1-and-des <pbe-cipher-spi>)
+)
---
+++ test/tests/rsa/pkcs/%3a5.scm
@@ -0,0 +1,85 @@
+(import (rnrs)
+	(sagittarius)
+	(rsa pkcs :5)
+	(crypto)
+	(srfi :64 testing))
+
+(test-begin "PKCS#5 tests")
+
+;; key derivation tests
+(define (test-derive-key expected password salt count key-length)
+  (let ((d-key (derive-key (string->utf8 password)
+			   (string->utf8 salt)
+			   count
+			   key-length)))
+    (test-equal (format "derive key(password ~a salt ~a count ~a length ~a)" 
+			password salt count key-length)
+		expected d-key)))
+
+;; The expected data is from RFC 6070
+(test-derive-key #vu8(#x0c #x60 #xc8 #x0f #x96 #x1f #x0e #x71
+		      #xf3 #xa9 #xb5 #x24 #xaf #x60 #x12 #x06
+		      #x2f #xe0 #x37 #xa6)
+		 "password" "salt" 1 20)
+
+(test-derive-key #vu8(#xea #x6c #x01 #x4d #xc7 #x2d #x6f #x8c
+		      #xcd #x1e #xd9 #x2a #xce #x1d #x41 #xf0
+		      #xd8 #xde #x89 #x57)
+		 "password" "salt" 2 20)
+
+(test-derive-key #vu8(#x4b #x00 #x79 #x01 #xb7 #x65 #x48 #x9a
+		      #xbe #xad #x49 #xd9 #x26 #xf7 #x21 #xd0
+		      #x65 #xa4 #x29 #xc1)
+		 "password" "salt" 4096 20)
+#|
+;; this is too much to round for Sagittarius...
+(test-derive-key #vu8(#xee #xfe #x3d #x61 #xcd #x4d #xa4 #xe4
+		      #xe9 #x94 #x5b #x3d #x6b #xa2 #x15 #x8c
+		      #x26 #x34 #xe9 #x84)
+		 "password" "salt" 16777216 20)
+|#
+
+(test-derive-key #vu8(#x3d #x2e #xec #x4f #xe4 #x1c #x84 #x9b
+		      #x80 #xc8 #xd8 #x36 #x62 #xc0 #xe4 #x4a
+		      #x8b #x29 #x1a #x96 #x4c #xf2 #xf0 #x70
+		      #x38)
+		 "passwordPASSWORDpassword"
+		 "saltSALTsaltSALTsaltSALTsaltSALTsalt" 4096 25)
+
+(test-derive-key #vu8(#x56 #xfa #x6a #xa7 #x55 #x48 #x09 #x9d
+		      #xcc #x37 #xd7 #xf0 #x34 #x25 #xe0 #xc3)
+		 "pass\x0;word" "sa\x0;lt"
+		 4096 16)
+
+;; encryption tests
+(define (test expected algorithm password salt count)
+  (let* ((param (make-pbe-parameter (string->utf8 salt) count))
+	 (key (generate-secret-key algorithm password))
+	 (pbe-cipher (cipher algorithm key :parameter param))
+	 (ciphertext (encrypt pbe-cipher 
+			      (string->utf8 "This is an example."))))
+    (test-equal algorithm expected ciphertext)))
+
+
+;; algorithm PBEWithMD5AndDES, password "password", salt "", count 1024
+(test #vu8(#xDD #xFA #x2D #x7E #x00 #x1E #xEE #x47 
+	   #x8E #x48 #x96 #x8A #xF6 #x3E #x12 #x67 
+	   #x77 #x25 #x32 #x5B #xD8 #x18 #xDA #x31)
+      pbe-with-md5-and-des "password" "" 1024)
+;; algorithm PBEWithMD5AndDES, password "password", salt "salt", count 1024
+(test #vu8(#x99 #x0C #x80 #x0D #x0A #xFB #xB8 #xEE 
+	   #x97 #x4A #x5B #x71 #x17 #xAA #x98 #x24 
+	   #x24 #x86 #x4B #x40 #x5A #x8B #xB4 #xBE)
+      pbe-with-md5-and-des "password" "salt" 1024)
+;; algorithm PBEwithSHA1AndDES, password "password", salt "", count 1024
+(test #vu8(#x15 #xAF #x01 #x45 #xF0 #xE0 #xCC #xFB 
+	   #xFB #x6E #xF3 #xE6 #x57 #x8D #xF2 #x64 
+	   #x59 #x92 #x78 #x72 #xCE #x63 #x08 #x5E)
+      pbe-with-sha1-and-des "password" "" 1024)
+;; algorithm PBEWithSha1AndDES, password "password", salt "salt", count 1024
+(test #vu8(#xFE #xE5 #x9B #xA8 #xA7 #xC0 #xF9 #x2B 
+	   #x21 #xC0 #xA1 #xD7 #x1C #x75 #xA9 #x81 
+	   #x93 #x3C #x10 #x34 #xEB #x4F #x5A #x17)
+      pbe-with-sha1-and-des "password" "salt" 1024)
+
+(test-end)
