7c6a1d3fcd24cfc3b8681f0021d48a5b64bab1ab
t.kato <devnull@localhost>
2012-08-13 15:57:21 +0200

    Added RFC 1421 PEM parser library (rfc pem).

---
+++ sitelib/rfc/pem.scm
@@ -0,0 +1,122 @@
+;;; -*- mode:scheme; coding: utf-8; -*-
+;;;
+;;; pem.scm - RFC 1421 PEM format parser library.
+;;;  
+;;;   Copyright (c) 2010-2012  Takashi Kato  <ktakashi@ymail.com>
+;;;   
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;   
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;  
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;  
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;  
+
+;; NOTE: this library handle only RFC 1421 format for now.
+;;  http://tools.ietf.org/html/rfc1421
+#< (sagittarius regex) >
+(library (rfc pem)
+    (export parse-pem
+	    parse-pem-file
+	    parse-pem-string)
+    (import (rnrs)
+	    (asn.1)
+	    (sagittarius)
+	    (sagittarius control)
+	    (sagittarius regex)
+	    (srfi :6 basic-string-ports)
+	    (srfi :8 receive)
+	    (srfi :13 strings)
+	    (srfi :26 cut)
+	    (rfc :5322)
+	    (rfc base64))
+
+  (define-condition-type &pem-error &error
+    make-pem-error pem-error?)
+
+  (define-condition-type &invalid-pem-format &pem-error
+    make-invalid-pem-format invalid-pem-format?)
+
+  (define (raise-pem-error ctr who msg . irr)
+    (raise (apply condition
+		  (filter values
+			  (list (ctr)
+				(and who (make-who-condition who))
+				(make-message-condition msg)
+				(make-irritants-condition irr))))))
+
+  (define (parse-pem in :key (multiple #f) (asn1 #f))
+    (define (parse-header header-string)
+      (rfc5322-read-headers (open-string-input-port header-string)))
+    (define (read-content p type)
+      (let ((header-out (open-output-string))
+	    (content-out (open-output-string)))
+	(let loop ((line (get-line p))
+		   (content? #f))
+	  (if (eof-object? line)
+	      (raise-pem-error make-invalid-pem-format
+			       'parse-pem "unexpected EOF")
+	      (cond ((#/-----END (.+?)-----/ line)
+		     => (^m 
+			 (unless (string=? type (m 1))
+			   (raise-pem-error make-invalid-pem-format 'parse-pem
+					    "invalid END clause found"
+					    `(expected ,type) `(got ,(m 1))))
+			 (values (parse-header (get-output-string header-out))
+				 (get-output-string content-out))))
+		    ((and (not content?) (string-scan line #\:))
+		     ;; As far as I know, this parameters are RFC 822 format
+		     (put-string header-out line)
+		     (put-string header-out "\r\n");; for rfc5322-read-headers
+		     (loop (get-line p) content?))
+		    (else
+		     (put-string content-out line)
+		     (loop (get-line p) #t)))))))
+    (define (rec in)
+      (let loop ((line (get-line in)))
+	(cond ((eof-object? line)
+	       (raise-pem-error make-invalid-pem-format 'parse-pem
+				"unexpected EOF appeared"))
+	      ((#/-----BEGIN (.+?)-----/ line)
+	       => (^m 
+		   (receive (params content) (read-content in (m 1))
+		     (let1 base64 (apply base64-decode-string content
+					 (if asn1 '(:transcoder #f) '()))
+		       (values params
+			       (if asn1
+				   (read-asn.1-object 
+				    (open-bytevector-input-port base64))
+				   base64))))))
+	      (else (loop (get-line p))))))
+
+    (let loop ((r '()))
+      (receive (params content) (rec in)
+	(cond ((and multiple (not (eof-object? (peek-char in))))
+	       (loop (cons (cons params content) r)))
+	      (multiple (reverse! (cons (cons params content) r)))
+	      (else (values params content))))))
+
+  ;; for convenience
+  (define (parse-pem-file file . opts)
+    (call-with-input-file file (cut apply parse-pem <> opts)))
+
+  (define (parse-pem-string str . opts)
+    (apply parse-pem (open-string-input-port str) opts))
+
+)
---
+++ test/tests/rfc/pem.scm
@@ -0,0 +1,29 @@
+(import (rnrs)
+	(sagittarius)
+	(sagittarius control)
+	(clos user)
+	(rfc pem)
+	(asn.1)
+	(srfi :64 testing))
+
+(test-begin "RFC PEM (RFC 1421) tests")
+
+(define test-file-1 (string-append (current-directory) "/test/data/pem-1.pem"))
+(define test-file-2 (string-append (current-directory) "/test/data/pem-2.pem"))
+
+(receive (header content) (parse-pem-file test-file-1)
+  (test-assert "PEM-1 header" (not (null? header)))
+  (test-equal "PEM-1 header count" 6 (length header))
+  (test-assert "PEM-1 content" (string? content)))
+
+(receive (header content) (parse-pem-file test-file-2 :asn1 #t)
+  (test-assert "PEM-2 header" (null? header))
+  (test-assert "PEM-2 content" (is-a? content <asn.1-encodable>)))
+
+(let ((r (parse-pem-file test-file-2 :multiple #t :asn1 #t)))
+  (test-equal "PEM-2 (multiple)" 2 (length r))
+  (dolist (l r)
+    (test-assert "PEM-2 (multiple) header" (null? (car l)))
+    (test-assert "PEM-2 (multiple) content" (is-a? (cdr l) <asn.1-encodable>))))
+
+(test-end)
