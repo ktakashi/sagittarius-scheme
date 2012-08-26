d018e8cae07ec746a2ebc4b466143317d7dc7f9e
t.kato <tk_riple@yahoo.co.jp>
2012-08-26 21:44:08 +0200

    Supported SRFI-29

---
+++ sitelib/srfi/%3a29/format.scm
@@ -0,0 +1,99 @@
+;;; -*- mode:scheme; coding: utf-8; -*-
+;;;
+;;; localization - SRFI-29 library.
+;;;  Taken from SRFI-29 reference implementation of format
+
+;;; Copyright (C) Scott G. Miller (2002). All Rights Reserved.
+;;; 
+;;; Permission is hereby granted, free of charge, to any person obtaining a
+;;; copy of this software and associated documentation files (the "Software"),
+;;; to deal in the Software without restriction, including without limitation
+;;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;;; and/or sell copies of the Software, and to permit persons to whom the
+;;; Software is furnished to do so, subject to the following conditions:
+;;; 
+;;; The above copyright notice and this permission notice shall be included in 
+;;; ll copies or substantial portions of the Software.
+;;; 
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+;;; THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+;;; IN THE SOFTWARE.
+#!compatible
+(library (srfi :29 format)
+    (export format)
+    (import (rnrs) (srfi :6 basic-string-ports))
+
+  ;;An SRFI-28 and SRFI-29 compliant version of format.  It requires
+  ;;SRFI-23 for error reporting.
+  (define format
+    (lambda (format-string . objects)
+      (let ((buffer (open-output-string)))
+	(let loop ((format-list (string->list format-string))
+		   (objects objects)
+		   (object-override #f))
+	  (cond ((null? format-list) (get-output-string buffer))
+		((char=? (car format-list) #\~)
+		 (cond ((null? (cdr format-list))
+			(error 'format "Incomplete escape sequence"))
+		       ((char-numeric? (cadr format-list))
+			(let posloop ((fl (cddr format-list))
+				      (pos (string->number
+					    (string (cadr format-list)))))
+			  (cond ((null? fl)
+				 (error 'format "Incomplete escape sequence"))
+				((and (eq? (car fl) '#\@)
+				      (null? (cdr fl)))
+				 (error 'format "Incomplete escape sequence"))
+				((and (eq? (car fl) '#\@)
+				      (eq? (cadr fl) '#\*))
+				 (loop (cddr fl) objects (list-ref objects pos)))
+				(else
+				 (posloop (cdr fl)
+					  (+ (* 10 pos)
+					     (string->number
+					      (string (car fl)))))))))
+		       (else
+			(case (cadr format-list)
+			  ((#\a)
+			   (cond (object-override
+				  (begin
+				    (display object-override buffer)
+				    (loop (cddr format-list) objects #f)))
+				 ((null? objects)
+				  (error 'format "No value for escape sequence"))
+				 (else
+                                  (begin
+                                    (display (car objects) buffer)
+                                    (loop (cddr format-list)
+                                          (cdr objects) #f)))))
+			  ((#\s)
+			   (cond (object-override
+				  (begin
+				    (display object-override buffer)
+				    (loop (cddr format-list) objects #f)))
+				 ((null? objects)
+				  (error 'format "No value for escape sequence"))
+				 (else
+                                  (begin
+                                    (write (car objects) buffer)
+                                    (loop (cddr format-list)
+                                          (cdr objects) #f)))))
+			  ((#\%)
+			   (if object-override
+			       (error 'format "Escape sequence following positional override does not require a value"))
+			   (display #\newline buffer)
+			   (loop (cddr format-list) objects #f))
+			  ((#\~)
+			   (if object-override
+			       (error 'format "Escape sequence following positional override does not require a value"))
+			   (display #\~ buffer)
+			   (loop (cddr format-list) objects #f))
+			  (else
+                           (error 'format "Unrecognized escape sequence"))))))
+		(else (display (car format-list) buffer)
+		      (loop (cdr format-list) objects #f)))))))
+)
---
+++ sitelib/srfi/%3a29/localization.scm
@@ -0,0 +1,93 @@
+;;; -*- mode: scheme; coding: utf-8; -*-
+;;;
+;;; srfi-29 - implementation of SRFI-28
+;;;  
+;;;   Copyright (c) 2010-2012  Takashi Kato  <ktakashi@ymail.com>
+;;;   
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;   
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;  
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;  
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;  
+#!compatible
+#< (sagittarius regex) >
+(library (srfi :29 localization)
+    (export current-language current-country current-locale-details
+	    declare-bundle! store-bundle! load-bundle!
+	    localized-template
+	    format)
+    (import (rnrs)
+	    (only (sagittarius) getenv make-equal-hashtable)
+	    (sagittarius object)
+	    (sagittarius regex)
+	    (only (util hashtables) alist->hashtable)
+	    (srfi :29 format)
+	    (srfi :39 parameters))
+
+;; bundle specifiers are (package ... [country] lang)
+(define *bundles* (make-equal-hashtable))
+
+;; implement as parameters (default to something other than en?)
+(define current-language (make-parameter 'en))
+(define current-country (make-parameter 'us))
+(define current-locale-details (make-parameter '()))
+
+;; should this be default?
+(define default-language 'en)
+
+;; initialize locale from LANG env variable if defined
+(let ((lang (getenv "LANG")))
+  (cond ((and lang (#/^([a-z]+)(?:[-_](\w+))?(?:\.(.*))?$/ lang))
+         => (lambda (m)
+              (current-language (string->symbol (m 1)))
+              (cond ((m 2)
+                     => (lambda (x)
+                          (current-country
+                           (string->symbol (string-downcase x))))))
+              (cond ((m 3)
+                     => (lambda (x)
+                          (current-locale-details
+                           (list (string->symbol (string-downcase x)))))))))))
+
+;; possibly tie these in with text.gettext
+(define (load-bundle! bundle-specifier) #f)
+(define (store-bundle! bundle-specifier) #f)
+
+;; declare a bundle of templates with a given bundle specifier
+(define (declare-bundle! bundle-specifier bundle-assoc-list)
+  (set! (~ *bundles* bundle-specifier) (alist->hashtable bundle-assoc-list)))
+
+;; lookup a name in a given package
+(define (localized-template package-name template-name)
+  (define (rdc ls)
+    (cond ((null? ls) '())
+          ((null? (cdr ls)) '())
+          (else (cons (car ls) (rdc (cdr ls))))))
+
+  (let loop ((name (list package-name (current-language) (current-country))))
+    (let ((bundle (~ *bundles* name)))
+      (or (and bundle (~ bundle template-name))
+          (let ((next (rdc name)))
+            (or (and (pair? next) (loop next))
+		(let ((ht (~ *bundles* (list package-name default-language))))
+		  (and ht (~ ht template-name)))))))))
+
+)
