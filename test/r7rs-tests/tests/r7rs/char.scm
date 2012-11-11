25fdc0cc24bad608a566b8efa9d0f891adf776ed
t.kato <devnull@localhost>
2012-11-11 09:01:58 +0100

    Added digit-value test.

--- test/r7rs-tests/tests/r7rs/char.scm
+++ test/r7rs-tests/tests/r7rs/char.scm
@@ -13,6 +13,7 @@
       (test-equal 3 (digit-value #\3))
       (test-equal 4 (digit-value #\x0664))
       (test-false (digit-value #\x0EA6))
+      (test-equal 0 (digit-value #\x0AE6))
 
       (test-equal "abdegh" (string-map char-foldcase "AbdEgH"))
       (test-equal "StUdLyCaPs" (string-map
