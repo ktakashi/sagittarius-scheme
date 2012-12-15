5a07b2fd891782ed2ed91dfdf00b62c1786b3a6d
t.kato <devnull@localhost>
2012-12-15 09:27:24 +0100

    Modified SRFI-42 to run on R6RS mode.

--- test/tests/sagittarius.scm
+++ test/tests/sagittarius.scm
@@ -762,4 +762,10 @@
 (test-assert "(eqv? 1.0+0.0i 1.0-0.0i)" (not (eqv? 1.0+0.0i 1.0-0.0i)))
 (test-assert "(eqv? 1.0+0i 1.0-0i)" (eqv? 1.0+0i 1.0-0i))
 
+;; to use SRFI-42 in R6RS mode
+#!r6rs
+(import (srfi :42))
+(test-assert "not keyword" (symbol? ':foo))
+(test-equal "R6RS mode SRFI-42" '(0 1 2 3 4 5 6 7 8 9) (list-ec (: x 10) x))
+
 (test-end)
