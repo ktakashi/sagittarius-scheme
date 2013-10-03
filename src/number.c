c0451bc40067544a29519be3bc07220e1d1a1258
t.kato <devnull@localhost>
2013-10-03 10:42:03 +0200

    Changed file header style.

--- src/number.c
+++ src/number.c
@@ -1,6 +1,4 @@
-/* -*- C -*- */
-/*
- * number.c
+/* number.c                                        -*- mode:c; coding:utf-8; -*-
  *
  *   Copyright (c) 2010-2013  Takashi Kato <ktakashi@ymail.com>
  *
