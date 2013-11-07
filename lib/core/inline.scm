a4a08058a98afde5669b1e3c737f2fae160ffc58
t.kato <devnull@localhost>
2013-11-07 13:30:16 +0100

    Commented out raw compiler macro (haven't decided how it should be...)

--- lib/core/inline.scm
+++ lib/core/inline.scm
@@ -1,13 +1,16 @@
 ;; part of Sagittarius Scheme
 (library (core inline)
     (export define-inliner
-	    define-raw-inliner)
+	    ;; not export this for now.
+	    ;;define-raw-inliner
+	    )
     (import (core)
 	    (core base)
 	    (core syntax)
 	    (sagittarius)
 	    (sagittarius compiler)
 	    (sagittarius compiler procedure)
+	    (sagittarius compiler util)
 	    (sagittarius vm))
 
   ;; trick to get pass1 from compiler
@@ -17,13 +20,24 @@
   (define-syntax define-raw-inliner
     (lambda (x)
       (syntax-case x ()
-	((k name library (form pass1*) body ...)
+	((k name library proc)
 	 (with-syntax ((debug-name (datum->syntax #'k
 				    (string->symbol (format "inliner/~a"
 							    (datum name))))))
 	   #'(let ((debug-name (lambda (form p1env)
-				 (define (pass1* expr) (pass1 expr p1env))
-				 body ...)))
+				 (define (const-value expr)
+				   (let ((iform (pass1 expr p1env)))
+				     ;; $CONST = #($CONST value)
+				     (if (eqv? (vector-ref iform 0) $CONST)
+					 (vector-ref iform 1)
+					 (undefined))))
+				 (let ((form2 (proc form const-value)))
+				   (if (undefined? form2)
+				       ;; return undefined so that compiler
+				       ;; just compiles to $call
+				       form2
+				       ;; must return iform
+				       (pass1 form2 p1env))))))
 	       (procedure-inliner-set! (find-procedure 'name 'library)
 				       debug-name)))))))
 
@@ -40,9 +54,10 @@
 	((_ name lib pattern* ...)
 	 (with-syntax ((((pattern fender template) ...) 
 			(parse #'(pattern* ...) '())))
-	   #'(define-raw-inliner name lib (form pass1)
-	       (syntax-case form ()
-		 (pattern fender (pass1 (syntax template))) ...
-		 (_ (undefined)))))))))
+	   #'(define-raw-inliner name lib
+	       (lambda (form const-value)
+		 (syntax-case form ()
+		   (pattern fender (syntax template)) ...
+		   (_ (undefined))))))))))
 
 )
