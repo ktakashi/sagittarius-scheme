81e0053f12b90e021e01f1f9652961a90973ecd7
t.kato <devnull@localhost>
2013-11-21 13:34:39 +0100

    Added public exponent slot to dsa-private-key Added private key import for DSA.

--- ext/crypto/crypto/key/dsa.scm
+++ ext/crypto/crypto/key/dsa.scm
@@ -69,14 +69,16 @@
     (format out "   q: ~x~%" (slot-ref pr 'q))
     (format out "   g: ~x~%" (slot-ref pr 'g))
     (format out "   Y: ~x~%>" (slot-ref pr 'Y)))
-
+  ;; should we extends public key so that verify can be done with private key?  
   (define-class <dsa-private-key> (<private-key> <dsa-key-parameter>)
-    ((X :init-keyword :X)))
+    ((Y :init-keyword :Y :init-value #f)
+     (X :init-keyword :X)))
   (define-method write-object ((pr <dsa-private-key>) out)
     (format out "#<dsa-private-key~%")
     (format out "   p: ~x~%" (slot-ref pr 'p))
     (format out "   q: ~x~%" (slot-ref pr 'q))
     (format out "   g: ~x~%" (slot-ref pr 'g))
+    (format out "   Y: ~x~%" (slot-ref pr 'Y))
     (format out "   X: ~x~%>" (slot-ref pr 'X)))
 
   (define-method generate-key-pair ((m <dsa>) :key (size 1024)
@@ -89,7 +91,7 @@
 	   (q (slot-ref pr 'q))
 	   (g (slot-ref pr 'g)))
       (let-values (((x y) (generate-dsa-keypair p q g :prng prng)))
-	(let ((private (make <dsa-private-key> :p p :q q :g g :X x))
+	(let ((private (make <dsa-private-key> :p p :q q :g g :Y y :X x))
 	      (public (make <dsa-public-key> :p p :q q :g g :Y y)))
 	  (make-keypair private public)))))
 
@@ -99,19 +101,18 @@
   ;;   r INTEGER,
   ;;   s INTEGER
   ;; }
-  (define (dsa-sign bv key :key (hasher :hash #f) (prng (secure-random RC4)))
-    (define (get-digester p)
-      (if hasher
-	  hasher
-	  (cond ((assv (bitwise-length p) +key-length+) => cdr)
-		(else (error 'dsa-sign "can't determine the hash algorithm")))))
+  (define (get-digester who p)
+    (cond ((assv (bitwise-length p) +key-length+) => cdr)
+	  (else (error who "can't determine the hash algorithm"))))
+
+  (define (dsa-sign bv key :key (prng (secure-random RC4)))    
     (unless (is-a? key <dsa-private-key>)
       (raise-encrypt-error 'dsa-sign "invalid key" 'DSA))
     (let* ((p (slot-ref key 'p))
 	   (q (slot-ref key 'q))
 	   (g (slot-ref key 'g))
 	   (x (slot-ref key 'X))
-	   (digester (get-digester p))
+	   (digester (get-digester 'dsa-sign p))
 	   (k (random prng q))
 	   (r (mod (mod-expt g k p) q))
 	   (xr+h (mod (+ (bytevector->integer (hash digester bv)) (* x r)) q))
@@ -121,7 +122,31 @@
 	       (make-der-integer r)
 	       (make-der-integer s)))))
 
-  (define (dsa-verify m s key))
+  (define (dsa-verify M S key)
+    (unless (is-a? key <dsa-public-key>) (error 'dsa-verify "invalid key"))
+    ;; signature must be the DER encoded object with r and s
+    ;; if the S is not an ASN.1 object then reader will raise an error :)
+    (let ((r&s (read-asn.1-object (open-bytevector-input-port S))))
+      (unless (and (is-a? r&s <asn.1-sequence>)
+		   (= 2 (length (slot-ref r&s 'sequence))))
+	(error 'dsa-verify "invalid signature"))
+      (let ((r (der-integer->integer (car (slot-ref r&s 'sequence))))
+	    (s (der-integer->integer (cadr (slot-ref r&s 'sequence))))
+	    (q (slot-ref key 'q))
+	    (p (slot-ref key 'p))
+	    (g (slot-ref key 'g))
+	    (y (slot-ref key 'Y)))
+	(unless (and (< 0 r q) (< 0 s q)) 
+	  (error 'dsa-verify "invalid signature"))
+	(let* ((digester (get-digester 'dsa-verify p))
+	       (w  (mod-inverse s q))
+	       (u1 (mod (* (bytevector->integer (hash digester M)) w) q))
+	       (u2 (mod (* r w) q))
+	       (u1d (mod-expt g u1 p))
+	       (u2d (mod-expt y u2 p))
+	       (v  (mod (mod (* u1d u2d) p) q)))
+	  (or (= v r)
+	      (error 'dsa-verify "inconsistent"))))))
 
   ;; cipher
   (define-class <dsa-cipher-spi> (<cipher-spi>) ())
@@ -161,8 +186,11 @@
         publicExponent INTEGER
       }
   |#
+  ;; For DSA public key, we need 'Public Key' structure.
   ;; I think this is a bad format since sign and verify always need
   ;; p, q and g. but it's like this...
+  ;; If every one uses publicExponent + parameter format
+  ;; I would go for that one though...
   (define oid (make-der-object-identifier "1.2.840.10040.4.1"))
   (define-method export-public-key ((m <dsa>) (key <dsa-public-key>))
     (encode (make-der-sequence
@@ -180,59 +208,66 @@
   (define-method import-public-key ((marker <dsa>) (in <port>))
     (import-public-key DSA (read-asn.1-object in)))
   (define-method import-public-key ((marker <dsa>) (in <asn.1-sequence>))
-    (let ((objects (slot-ref in 'sequence)))
-      (unless (= 2 (length objects))
-	(assertion-violation 'import-public-key "bad sequence size" in))
-      (unless (and (is-a? (car objects) <asn.1-sequence>)
-		   (is-a? (cadr objects) <der-bit-string>))
-	(assertion-violation 'import-public-key "bad component" in))
-      (let ((params (slot-ref (car objects) 'sequence)))
-	(unless (= 2 (length params))
-	  (assertion-violation 'import-public-key "bad sequence size" in))
-	(unless (and (and (is-a? (car params) <der-object-identifier>)
-			  (equal? (car params) oid))
-		     (is-a? (cadr params) <asn.1-sequence>))
-	  (assertion-violation 'import-public-key "bad component" in))
-	(let ((ber-pqg (slot-ref (cadr params) 'sequence))
-	      (Y (read-asn.1-object 
-		  (open-bytevector-input-port (slot-ref (cadr objects) 'data))))
-	      )
-	  (unless (is-a? Y <der-integer>)
-	    (assertion-violation 'import-public-key "bad component" in))
-	  (make <dsa-public-key>
-	    :p (bytevector->integer (slot-ref (car ber-pqg) 'bytes))
-	    :q (bytevector->integer (slot-ref (cadr ber-pqg) 'bytes))
-	    :g (bytevector->integer (slot-ref (caddr ber-pqg) 'bytes))
-	    :Y (bytevector->integer (slot-ref Y 'bytes)))))))
+    (define (extract-param&keys who in)
+      (let ((objects (slot-ref in 'sequence)))
+	(unless (= 2 (length objects))
+	  (assertion-violation who "bad sequence size" in))
+	(unless (and (is-a? (car objects) <asn.1-sequence>)
+		     (is-a? (cadr objects) <der-bit-string>))
+	  (assertion-violation who "bad component" in))
+	(let ((params (slot-ref (car objects) 'sequence)))
+	  (unless (= 2 (length params))
+	    (assertion-violation who "bad sequence size" in))
+	  (unless (and (and (is-a? (car params) <der-object-identifier>)
+			    (equal? (car params) oid))
+		       (is-a? (cadr params) <asn.1-sequence>))
+	    (assertion-violation who "bad component" in))
+	  (let ((ber-pqg (slot-ref (cadr params) 'sequence))
+		(k (read-asn.1-object 
+		    (open-bytevector-input-port 
+		     (slot-ref (cadr objects) 'data)))))
+	    (unless (is-a? k <der-integer>)
+	      (assertion-violation who "bad component" in))
+	    (values (der-integer->integer (car ber-pqg))
+		    (der-integer->integer (cadr ber-pqg))
+		    (der-integer->integer (caddr ber-pqg))
+		    (der-integer->integer k))))))
+    (let-values (((p q g Y) (extract-param&keys 'import-public-key in)))
+      (make <dsa-public-key> :p p :q q :g g :Y Y)))
   #|
-      PrivateKeyInfo ::= SEQUENCE {
+      // From OpenSSL
+      DSAPrivateKey ::= OCTETSTRING {
         version Version,
-        algorithm AlgorithmIdentifier,
-        PrivateKey OCTETSTRING
-      }
-      AlgorithmIdentifier ::= SEQUENCE {
-        algorithm ALGORITHM.id,
-        parameters Dss-Parms
-      }
-      Dss-Parms ::= SEQUENCE {
         p INTEGER,
         q INTEGER,
-        g INTEGER
-      }
-      DSAPrivateKey ::= OCTETSTRING {
+        g INTEGER,
+        publicExponent  INTEGER,
         privateExponent INTEGER
       }
   |#
   (define-method export-private-key ((m <dsa>) (key <dsa-private-key>))
+    ;; should we do like this or OpenSSL DSA private key format?
     (encode (make-der-sequence
-	     (make-der-sequence
-	      oid
-	      (make-der-sequence
-	       (make-der-integer (slot-ref key 'p))
-	       (make-der-integer (slot-ref key 'q))
-	       (make-der-integer (slot-ref key 'g))))
-	     (make-der-object-identifier 
-	      (encode (make-der-integer (slot-ref key 'X)))))))
+	     (make-der-integer 0)
+	     (make-der-integer (slot-ref key 'p))
+	     (make-der-integer (slot-ref key 'q))
+	     (make-der-integer (slot-ref key 'g))
+	     (make-der-integer (slot-ref key 'Y))
+	     (make-der-integer (slot-ref key 'X)))))
 
-
+  (define-method import-private-key ((marker <dsa>) (in <bytevector>))
+    (import-private-key DSA (open-bytevector-input-port in)))
+  (define-method import-private-key ((marker <dsa>) (in <port>))
+    (import-private-key DSA (read-asn.1-object in)))
+  (define-method import-private-key ((marker <dsa>) (in <asn.1-sequence>))
+    (let ((objs (slot-ref in 'sequence)))
+      (unless (= 6 (length objs)) 
+	(error 'import-private-key "invalid sequence size" in))
+      (let ((pqgYX (cdr objs)))
+	(make <dsa-private-key>
+	  :p (der-integer->integer (car pqgYX))
+	  :q (der-integer->integer (cadr pqgYX))
+	  :g (der-integer->integer (caddr pqgYX))
+	  :Y (der-integer->integer (cadddr pqgYX))
+	  :X (der-integer->integer (car (cddddr pqgYX)))))))
 )
