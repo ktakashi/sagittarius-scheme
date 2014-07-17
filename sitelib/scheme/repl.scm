c44e327361b7321c597d96662b4cccbf46c80342
t.kato <devnull@localhost>
2014-07-17 11:32:36 +0200

    Fixed interaction-environment. (it was a variable but should be a procedure)

--- sitelib/scheme/repl.scm
+++ sitelib/scheme/repl.scm
@@ -4,5 +4,5 @@
     (export interaction-environment)
     (import (only (rnrs) define quote)
 	    (only (sagittarius vm) find-library))
-  (define interaction-environment (find-library 'user #t))
+  (define (interaction-environment) (find-library 'user #t))
 )
