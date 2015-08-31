657570562876dbe22c40f779a28bec4f89927434
ktakashi <ktakashi@ymail.com>
2015-08-31 14:22:50 +0200

    Adding rather stupid workaround to show error message.

--- src/vm.c
+++ src/vm.c
@@ -60,6 +60,7 @@
 #include "sagittarius/gloc.h"
 #include "sagittarius/weak.h"
 #include "sagittarius/thread.h"
+#include "sagittarius/unicode.h"
 
 static SgInternalMutex global_lock;
 
@@ -527,7 +528,16 @@ static inline void report_error(SgObject exception, SgObject out)
       }
     }
   }
-  Sg_Write(Sg_GetStringFromStringPort(buf), out, SG_WRITE_DISPLAY);
+  /* for some reason, certain Windows platform failed to create
+     stdout, at that moment, there is no stderr ready so out might
+     be NULL. */
+  if (out && SG_PORTP(out)) {
+    Sg_Write(Sg_GetStringFromStringPort(buf), out, SG_WRITE_DISPLAY);
+  } else {
+    /* Ok, something is wrong, so just dump to CRT stderr */
+    SgObject m = Sg_GetStringFromStringPort(buf);
+    fprintf(stderr, "%s\n", Sg_Utf32sToUtf8s(m));
+  }
   Sg_FlushAllPort(FALSE);
 }
 
