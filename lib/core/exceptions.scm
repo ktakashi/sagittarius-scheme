5cc94ffca2e5a5bb250e84eb9712e6cb30f88dc8
ktakashi <ktakashi@ymail.com>
2015-09-11 22:22:39 +0200

    Fixing non tail-call on <body>

--- lib/core/exceptions.scm
+++ lib/core/exceptions.scm
@@ -15,36 +15,31 @@
 	((_ (var clause ... (else e1 e2 ...)) b1 b2 ...)
 	 #'((call/cc
 	     (lambda (guard-k)
-	       (with-exception-handler
-		(lambda (condition)
-		  (guard-k
-		   (lambda ()
-		     (let ((var condition))
-		       (cond clause ... 
-			     (else e1 e2 ...))))))
-		(lambda ()
-		  ;; Sagittarius prefer receive
-		  (receive args
-		      (let () b1 b2 ...)
-		    (guard-k (lambda () (apply values args))))))))))
+	       (lambda ()
+		 (with-exception-handler
+		  (lambda (condition)
+		    (guard-k
+		     (lambda ()
+		       (let ((var condition))
+			 (cond clause ... 
+			       (else e1 e2 ...))))))
+		  (lambda () b1 b2 ...)))))))
 	((_ (var clause ...) b1 b2 ...)
 	 #'((call/cc
 	     (lambda (guard-k)
-	       (with-exception-handler
-		(lambda (condition)
-		  ((call/cc
-		    (lambda (handler-k)
-		      (guard-k
-		       (lambda ()
-			 (let ((var condition))
-			   (cond clause ...
-				 (else 
-				  (handler-k 
-				   (lambda () 
-				     (raise-continuable condition))))))))))))
-		(lambda ()
-		  (receive args
-		      (let () b1 b2 ...)
-		    (guard-k (lambda () (apply values args)))))))))))))
+	       (lambda ()
+		 (with-exception-handler
+		  (lambda (condition)
+		    ((call/cc
+		      (lambda (handler-k)
+			(guard-k
+			 (lambda ()
+			   (let ((var condition))
+			     (cond clause ...
+				   (else 
+				    (handler-k 
+				     (lambda () 
+				       (raise-continuable condition))))))))))))
+		  (lambda () b1 b2 ...))))))))))
 
 )
