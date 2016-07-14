58ef0165b7bd73499b8243e0d9e51968a1a28f4a
ktakashi <ktakashi@ymail.com>
2016-07-14 12:11:29 +0200

    Adding more tests.

--- test/tests/sagittarius/remote-repl.scm
+++ test/tests/sagittarius/remote-repl.scm
@@ -9,20 +9,27 @@
 
 (test-begin "Remote REPL")
 
-(define-values (thunk server) (make-remote-repl "8501" :log #f))
+(define-values (thunk server) (make-remote-repl "10000" :log #f))
 (define t (thread-start! (make-thread thunk)))
 
-(define commands "#!read-macro=sagittarius/regex #/aaa/")
-
-(define (test-repl command)
+(define (test-repl-port in)
   (let-values (((out extract) (open-string-output-port)))
-    (parameterize ((current-input-port (open-string-input-port commands))
+    (parameterize ((current-input-port in)
 		   (current-prompter (lambda () #t))
 		   (current-output-port out))
       (connect-remote-repl "localhost" "8501" :quiet #t))
     (extract)))
+(define (test-repl command) (test-repl-port (open-string-input-port command)))
 
-(test-equal "hashbang on remote REPL" "#/aaa/\n" (test-repl commands))
+(test-equal "basics (symbol)" "a\n" (test-repl "'a"))
+(test-equal "basics (number)" "123\n" (test-repl "123"))
+(test-equal "definition(1)" "#<unspecified>\n" (test-repl "(define a 'b)"))
+(test-equal "definition(2)" "b\n" (test-repl "a"))
+
+(test-equal "hashbang on remote REPL" "#/aaa/\n"
+	    (test-repl "#!read-macro=sagittarius/regex #/aaa/"))
+(test-equal "hashbang on remote REPL(2)" "#vu8(97 97 97)\n"
+	    (test-repl "#!read-macro=sagittarius/bv-string #*\"aaa\""))
 
 (socket-close server)
 (guard (e (else #t)) (thread-join! t))
