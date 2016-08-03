575c2634276c9fe241e6ab7cfb642966cf30cb7e
ktakashi <ktakashi@ymail.com>
2016-08-03 10:23:06 +0200

    Adding high level APIs for websocket.

--- sitelib/rfc/websocket.scm
+++ sitelib/rfc/websocket.scm
@@ -31,10 +31,24 @@
 ;; Websocket client library. Server library may come in (rfc websocket server)
 ;; but not promised.
 (library (rfc websocket)
-  (export make-websocket-connection
+  (export ;; User level APIs
+	  make-websocket
+	  websocket?
+	  websocket-open
+	  websocket-close
+	  websocket-send
+	  websocket-ping
+	  websocket-on-text-message
+	  websocket-on-binary-message
+	  websocket-on-open
+	  websocket-on-error
+	  websocket-on-close
+	  
+	  ;; Low APIs
+	  make-websocket-connection
 	  websocket-connection?
 	  websocket-connection-handshake!
-	  ;; APIs
+	  
 	  websocket-send-text
 	  websocket-send-binary
 	  websocket-send-close
@@ -44,16 +58,158 @@
 	  websocket-receive
 	  websocket-compose-close-status
 	  websocket-parse-close-status
-
+	  
+	  +websocket-text-frame+
+	  +websocket-binary-frame+
+	  +websocket-close-frame+
+	  +websocket-ping-frame+
+	  +websocket-pong-frame+
+	  
 	  ;; conditions
 	  websocket-error?
 	  websocket-engine-error?
 	  websocket-closed-error?
 	  websocket-error-status
 	  websocket-error-message
+
+	  websocket-pong-error?
+	  websocket-error-pong-data
 	  
 	  *websocket-mask-data?*
 	  )
-  (import (rfc websocket conditions)
+  (import (rnrs)
+	  (srfi :18)
+	  (rfc websocket conditions)
 	  (rfc websocket connection)
-	  (rfc websocket messages)))
+	  (rfc websocket messages)
+	  (util concurrent shared-queue))
+
+(define-record-type websocket
+  (fields protocols
+	  extensions
+	  connection
+	  dispatchers
+	  (mutable thread)
+	  mutex
+	  pong-channel)
+  (protocol
+   (lambda (p)
+     (lambda (uri :key (protocols '()) (extensions '()) (engine 'http))
+       (p protocols extensions
+	  (make-websocket-connection uri engine)
+	  (make-eq-hashtable)
+	  #f
+	  (make-mutex)
+	  (make-shared-queue 1))))))
+(define-condition-type &websocket-pong &websocket
+  make-websocket-pong-error websocket-pong-error?
+  (pong-data websocket-error-pong-data))
+
+(define (invoke-event websocket event . opt)
+  (define dispatchers (websocket-dispatchers websocket))
+  (cond ((hashtable-ref dispatchers event #f) =>
+	 (lambda (handler) (apply handler websocket opt))))
+  websocket)
+
+;; if it's &websocket, then let on-error handle it.
+;; others just go though.
+(define-syntax with-error-handling
+  (syntax-rules ()
+    ((_ websocket exprs ...)
+     (guard (e ((websocket-error? e)
+		(invoke-event websocket 'error e)
+		websocket))
+       exprs ...))))
+
+(define-syntax define-websocket
+  (syntax-rules ()
+    ((_ (name websocket . args) exprs ...)
+     (define (name websocket . args)
+       (with-error-handling websocket exprs ...)))))
+
+(define (start-dispatch-thread websocket)
+  (define (dispatch)
+    (define conn (websocket-connection websocket))
+    (define finish? #f)
+    (let restart ()
+      (guard (e ((websocket-closed-error? e) (raise e))
+		((websocket-error? e) (invoke-event websocket 'error e)))
+	(let loop ()
+	  (let-values (((opcode data) (websocket-receive conn)))
+	    (cond ((eqv? opcode +websocket-close-frame+)
+		   (set! finish? #t))
+		  ((eqv? opcode +websocket-text-frame+)
+		   (invoke-event websocket 'text data)
+		   (loop))
+		  ((eqv? opcode +websocket-binary-frame+)
+		   (invoke-event websocket 'binary data)
+		   (loop))
+		  ((eqv? opcode +websocket-pong-frame+)
+		   (shared-queue-put!
+		    (websocket-pong-channel websocket) data)
+		   (loop))
+		  (else
+		   ;; TODO should we raise an error?
+		   (websocket-send-close conn
+		    (websocket-compose-close-status 1002) #f)
+		   (loop))))))
+      (unless finish? (restart))))
+  (let ((t (make-thread dispatch)))
+    (websocket-thread-set! websocket t)
+    (thread-start! t)))
+
+(define-websocket (websocket-open websocket)
+  (websocket-connection-handshake! (websocket-connection websocket)
+				   (websocket-protocols websocket)
+				   (websocket-extensions websocket))
+  (start-dispatch-thread websocket)
+  (invoke-event websocket 'open))
+
+(define-websocket (websocket-close websocket
+				   :optional (status #f) (message ""))
+  (let ((data (cond (status (websocket-compose-close-status status message))
+		    (else #vu8()))))
+    ;; we don't wait, let dispatch thread handle it
+    (websocket-send-close (websocket-connection websocket) data #f)
+    (guard (e ((uncaught-exception? e) (raise (uncaught-exception-reason e))))
+      (thread-join! (websocket-thread websocket))))
+  (invoke-event websocket 'close))
+
+(define-websocket (websocket-send websocket data . opt)
+  (define conn (websocket-connection websocket))
+  (cond ((string? data) (apply websocket-send-text conn data opt))
+	((bytevector? data) (apply websocket-send-binary conn data opt))
+	(else (assertion-violation 'websocket-send "invalid data" data)))
+  websocket)
+
+(define-websocket (websocket-ping websocket data . opt)
+  (define mutex (websocket-mutex websocket))
+  (let try ()
+    (mutex-lock! mutex)
+    (websocket-send-ping (websocket-connection websocket) data)
+    (let ((r (apply shared-queue-get! (websocket-pong-channel websocket) opt)))
+      (mutex-unlock! mutex)
+      ;; Should we raise an exception?
+      ;; if so, what kind of exception? &websocket would be caught
+      (unless (and (bytevector? r) (bytevector=? data r))
+	(raise (condition (make-websocket-pong-error r)
+			  (make-who-condition 'websocket-ping)
+			  (make-message-condition "unknown pong"))))
+      websocket)))
+
+(define (set-event-handler websocket event handler)
+  (define dispatchers (websocket-dispatchers websocket))
+  (hashtable-set! dispatchers event handler))
+
+(define (websocket-on-text-message websocket handler)
+  (set-event-handler websocket 'text handler))
+(define (websocket-on-binary-message websocket handler)
+  (set-event-handler websocket 'binary handler))
+(define (websocket-on-open websocket handler)
+  (set-event-handler websocket 'open handler))
+(define (websocket-on-error websocket handler)
+  (set-event-handler websocket 'error handler))
+(define (websocket-on-close websocket handler)
+  (set-event-handler websocket 'close handler))
+
+)				   
