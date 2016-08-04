739b8545572d4a0cf121485549cadf99c182c69d
ktakashi <ktakashi@ymail.com>
2016-08-04 09:49:38 +0200

    Adding license on websocket examples

--- example/websocket/chat-server.scm
+++ example/websocket/chat-server.scm
@@ -1,3 +1,33 @@
+;;; -*- mode:scheme; coding:utf-8; -*-
+;;;
+;;; example/websocket/chat-server.scm - WebSocket chat server
+;;;  
+;;;   Copyright (c) 2010-2016  Takashi Kato  <ktakashi@ymail.com>
+;;;   
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;   
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;  
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;  
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;  
+
 #!read-macro=sagittarius/bv-string
 #!read-macro=sagittarius/regex
 (import (rnrs)
@@ -77,7 +107,7 @@
 	(let ((out (buffered-port (socket-port socket #f) (buffer-mode block))))
 	  (websocket-send-frame! out op #t data #t))))
     (for-each send-it sockets))
-  
+
   (define (websocket-handler server socket)
     (define in/out (buffered-port (socket-port socket #f) (buffer-mode block)))
     (guard (e (else (cleanup socket)))
@@ -99,7 +129,10 @@
 					  (websocket-compose-close-status 1002)
 					  #t)
 		   (cleanup socket))))
-	  (handshake socket in/out))))
+	  (handshake socket in/out)))
+    ;; close the port. this is needed to prevent buffered-port flush
+    ;; during error reporting.
+    (close-port in/out))
 
   (define config (make-server-config :non-blocking? #t :use-ipv6? #t
 				     :exception-handler print))
