4d7aecebf699c4334f5ac8eda63803ac4d7e2a89
ktakashi <ktakashi@ymail.com>
2016-11-02 11:52:30 +0100

    Adding PBKDF-2 tests.

--- test/tests/rsa/pkcs/%3a5.scm
+++ test/tests/rsa/pkcs/%3a5.scm
@@ -17,6 +17,7 @@
 		decrypted)))
 )
 
+#!read-macro=sagittarius/bv-string
 (import (rnrs)
 	(sagittarius)
 	(rsa pkcs :5)
@@ -141,5 +142,19 @@
   (test-equal "PBES2 test" 
 	      "This is an example."
 	      (utf8->string (decrypt pbe-dec-cipher ciphertext))))
+
+;; PBKDF2 test vector
+;; From: https://www.ietf.org/rfc/rfc6070.txt
+(test-equal #vu8(#x0c #x60 #xc8 #x0f #x96 #x1f #x0e #x71 #xf3 #xa9 #xb5 #x24 #xaf #x60 #x12 #x06 #x2f #xe0 #x37 #xa6)
+	    (pbkdf-2 #*"password" #*"salt" 1 20))
+(test-equal #vu8(#xea #x6c #x01 #x4d #xc7 #x2d #x6f #x8c #xcd #x1e #xd9 #x2a #xce #x1d #x41 #xf0 #xd8 #xde #x89 #x57)
+	    (pbkdf-2 #*"password" #*"salt" 2 20))
+(test-equal #vu8(#x4b #x00 #x79 #x01 #xb7 #x65 #x48 #x9a #xbe #xad #x49 #xd9 #x26 #xf7 #x21 #xd0 #x65 #xa4 #x29 #xc1)
+	    (pbkdf-2 #*"password" #*"salt" 4096 20))
+;; This doesn't finish in practical time...
+#;(test-equal #vu8(#xee #xfe #x3d #x61 #xcd #x4d #xa4 #xe4 #xe9 #x94 #x5b #x3d #x6b #xa2 #x15 #x8c #x26 #x34 #xe9 #x84)
+	    (pbkdf-2 #*"password" #*"salt" 16777216 20))
+(test-equal #vu8(#x3d #x2e #xec #x4f #xe4 #x1c #x84 #x9b #x80 #xc8 #xd8 #x36 #x62 #xc0 #xe4 #x4a #x8b #x29 #x1a #x96 #x4c #xf2 #xf0 #x70 #x38)
+	    (pbkdf-2 #*"passwordPASSWORDpassword" #*"saltSALTsaltSALTsaltSALTsaltSALTsalt" 4096 25))
 
 (test-end)
