0eb926daefd67d4c1a4dfcd47fc42de6c5f1fcba
ktakashi <ktakashi@ymail.com>
2017-02-09 08:57:04 +0100

    Don't use default-random-source for random-source-state tests

--- test/tests/srfi/%3a27.scm
+++ test/tests/srfi/%3a27.scm
@@ -3,12 +3,6 @@
 	(srfi :64))
 
 
-(define (my-random-integer n)
-  (let ((x (random-integer n)))
-    (if (<= 0 x (- n 1))
-        x
-        (error "(random-integer n) returned illegal value" x))))
-
 (test-begin "SRFI 27")
 
 (test-assert "in range"
@@ -26,14 +20,22 @@
 		     (if (< 0 x 1)
 			 (loop (+ k 1) (* n 2))
 			 #f)))))
-
-(let* ((state1 (random-source-state-ref default-random-source))
-       (x1 (my-random-integer (expt 2 32)))
-       (state2 (random-source-state-ref default-random-source))
-       (x2 (my-random-integer (expt 2 32))))
-  (random-source-state-set! default-random-source state1)
-  (test-equal "state restore(1)" x1 (my-random-integer (expt 2 32)))
-  (random-source-state-set! default-random-source state2)
-  (test-equal "state restore(2)" x2 (my-random-integer (expt 2 32))))
+(let ()
+  (define my-random-source (make-random-source))
+  (define my-random-integer
+    (let ((random-integer (random-source-make-integers my-random-source)))
+      (lambda (n)
+	(let ((x (random-integer n)))
+	  (if (<= 0 x (- n 1))
+	      x
+	      (error "(random-integer n) returned illegal value" x))))))
+  (let ((state1 (random-source-state-ref my-random-source))
+	(x1 (my-random-integer (expt 2 32)))
+	(state2 (random-source-state-ref my-random-source))
+	(x2 (my-random-integer (expt 2 32))))
+    (random-source-state-set! my-random-source state1)
+    (test-equal "state restore(1)" x1 (my-random-integer (expt 2 32)))
+    (random-source-state-set! my-random-source state2)
+    (test-equal "state restore(2)" x2 (my-random-integer (expt 2 32)))))
 
 (test-end)
