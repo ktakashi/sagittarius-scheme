c9805afdc8af6acb4e2ee3dc628dee9c12dbf430
ktakashi <ktakashi@ymail.com>
2017-03-20 11:53:02 +0100

    Adding AES key wrap thing

--- sitelib/rfc/jwa.scm
+++ sitelib/rfc/jwa.scm
@@ -2,7 +2,7 @@
 ;;;
 ;;; rfc/jwa.scm - JSON Web Algorithms (JWK)
 ;;;  
-;;;   Copyright (c) 2010-2013  Takashi Kato  <ktakashi@ymail.com>
+;;;   Copyright (c) 2017  Takashi Kato  <ktakashi@ymail.com>
 ;;;   
 ;;;   Redistribution and use in source and binary forms, with or without
 ;;;   modification, are permitted provided that the following conditions
@@ -34,7 +34,11 @@
 	    jwa:make-ec-private-key
 	    jwa:make-rsa-public-key
 	    jwa:make-rsa-private-key
-	    jwa:make-rsa-crt-private-key)
+	    jwa:make-rsa-crt-private-key
+
+
+	    jwa:a128kw jwa:a192kw jwa:a256kw jwa:aes-key-wrap
+	    )
     (import (rnrs)
 	    (sagittarius)
 	    (crypto))
@@ -58,7 +62,27 @@
   (define (jwa:make-rsa-public-key n e) (generate-public-key RSA n e))
   (define (jwa:make-rsa-private-key n e d) (generate-private-key RSA n d))
   (define (jwa:make-rsa-crt-private-key n e d p q dp dq qi)
-    (generate-private-key RSA n d :public-exponent e :p p :q q))
+    (generate-private-key RSA n d :public-exponent e :p p :q q :dP dp
+			  :dQ dq :qP qi))
+
+  ;; Section 4.4
+  (define (jwa:a128kw wrapping-key key-material)
+    (unless (= (bytevector-length wrapping-key) 16)
+      (assertion-violation 'jwa:wrap-key/aes128
+			   "AES128KW requires 128 bit key"))
+    (jwa:aes-key-wrap wrapping-key key-material))
+  (define (jwa:a192kw wrapping-key key-material)
+    (unless (= (bytevector-length wrapping-key) 24)
+      (assertion-violation 'jwa:wrap-key/aes128
+			   "A192KW requires 192 bit key"))
+    (jwa:aes-key-wrap wrapping-key key-material))
+  (define (jwa:a256kw wrapping-key key-material)
+    (unless (= (bytevector-length wrapping-key) 32)
+      (assertion-violation 'jwa:wrap-key/aes128
+			   "A256KW requires 256 bit key"))
+    (jwa:aes-key-wrap wrapping-key key-material))
+  (define (jwa:aes-key-wrap wrapping-key key-material)
+    ((make-aes-key-wrap wrapping-key) key-material))
   
   )
 
--- sitelib/rfc/jwk.scm
+++ sitelib/rfc/jwk.scm
@@ -2,7 +2,7 @@
 ;;;
 ;;; rfc/jwk.scm - JSON Web Key (JWK)
 ;;;  
-;;;   Copyright (c) 2010-2013  Takashi Kato  <ktakashi@ymail.com>
+;;;   Copyright (c) 2017  Takashi Kato  <ktakashi@ymail.com>
 ;;;   
 ;;;   Redistribution and use in source and binary forms, with or without
 ;;;   modification, are permitted provided that the following conditions
