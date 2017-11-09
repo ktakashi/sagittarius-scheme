677b961f0ee2f3f7a3cc51f11fb508c39b46eaf4
ktakashi <ktakashi@ymail.com>
2017-11-09 13:35:24 +0100

    Adding combinators

---
+++ sitelib/sagittarius/combinators.scm
@@ -0,0 +1,139 @@
+;;; -*- mode:scheme; coding:utf-8; -*-
+;;;
+;;; sagittarius/combinators - Combinators
+;;;  
+;;;   Copyright (c) 2017  Takashi Kato  <ktakashi@ymail.com>
+;;;   
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;   
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;  
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;  
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;  
+
+
+;; $ suffix is taken from Gauche for partial application
+(library (sagittarius combinators)
+    (export compose (rename (compose .$))
+	    kestrels thrush ~> cardinal
+	    idiot starling
+	    ;; formal names
+	    (rename (idiot identity)
+		    (cardinal flip)
+		    (starling substitution))
+	    
+	    pa$ (rename (pa$ partial-apply$))
+	    apply$ map$ for-each$
+	    complement
+	    any-pred every-pred
+	    ;; R6RS uses exists and for-all
+	    (rename (any-pred exists-pred)
+		    (every-pred for-all-pred))
+	    )
+    (import (rnrs)
+	    (only (srfi :1) reverse!))
+
+(define (compose f g0 . g*)
+  (define (compose0 f g)
+    (lambda args
+      (let-values ((r (apply g args)))
+	(apply f r))))
+  (if (null? g*)
+      (compose0 f g0)
+      (let ((g1 (apply compose g0 g*)))
+	(compose0 f g1))))
+
+;; from https://github.com/raganwald-deprecated/homoiconic
+;;  and https://github.com/fantasyland/fantasy-birds
+;; Kestrels (K-Combinator) Kxy = x
+(define (kestrels x . x*)
+  (if (null? x*)
+      (lambda y x)
+      ;; not sure if this is correct...
+      (lambda y (apply values x x*))))
+
+(define (reverse-compose f f*)
+  (if (null? f*)
+      f
+      (apply compose (reverse! (cons f f*)))))
+;; Thrush Txy = yx
+(define (thrush x . x*)
+  (if (null? x*)
+      (lambda (f . f*)
+	((reverse-compose f f*) x))
+      (lambda (f . f*)
+	(apply (reverse-compose f f*) x x*))))
+
+;; threading macro (for convenience)
+(define-syntax ~>
+  (syntax-rules ()
+    ((_ x f f* ...)
+     ((thrush x) f f* ...))))
+
+;; Cardinal (C-Combinator) Cxyz = xzy
+(define (cardinal x0 . x*)
+  (lambda (y . y*)
+    (lambda (z0 . z*)
+      (define x (reverse-compose x0 x*))
+      (define z (reverse-compose z0 z*))
+      (let ((xz (x z)))
+	(if (null? y*)
+	    (xz y)
+	    (apply xz y y*))))))
+
+;; Starling (S-Combinator) Sxyz = xz(yz)
+(define (starling x0 . x*)
+  (lambda (y0 . y*)
+    (lambda (z0 . z*)
+      (define x (reverse-compose x0 x*))
+      (define y (reverse-compose y0 y*))
+      (define z (reverse-compose z0 z*))
+      ((x z) (y z)))))
+
+;; Idiot (I-Combinator, identity) Ix = x
+;; Should we define like this?
+;; (define (idiot a) a)
+(define idiot values)
+
+;; found in Gauche's document
+(define pa$
+  (case-lambda
+   ((proc) (lambda m (apply proc m)))
+   ((proc arg) (lambda m (apply proc arg m)))
+   ((proc arg . args)
+    (lambda m (apply proc arg (append args m))))))
+
+(define (apply$ proc) (pa$ apply proc))
+(define (map$ proc) (pa$ map proc))
+(define (for-each$ proc) (pa$ for-each proc))
+
+(define (complement pred) (lambda args (not (apply pred args))))
+(define (any-pred pred . pred*)
+  (lambda args
+    (or (apply pred args)
+	(and (not (null? pred*))
+	     (exists (lambda (p) (apply p args)) pred*)))))
+(define (every-pred pred . pred*)
+  (lambda args
+    (and (apply pred args)
+	 (or (null? pred*)
+	     (for-all (lambda (p) (apply p args)) pred*)))))
+
+)
---
+++ test/tests/sagittarius/combinators.scm
@@ -0,0 +1,98 @@
+(import (rnrs)
+	(sagittarius combinators)
+	(sagittarius io)
+	(srfi :26)
+	(srfi :64))
+
+(test-begin "Combinators")
+
+(define (value1->2 a) (values a a))
+(define (value1->3 a) (values a a a))
+(define (value2->1 a b) a)
+
+(test-equal '(1 1 1)
+	    (let-values ((r ((compose value1->3 value2->1 value1->2) 1))) r))
+
+(test-equal '(#(1)) ((compose list vector) 1)) 
+(test-equal '(#("1")) ((compose list vector string) #\1))
+
+(test-equal '#(1 2) ((compose list->vector list) 1 2)) 
+(test-equal '#(#\1 #\2) ((compose list->vector string->list string) #\1 #\2))
+
+(test-error (compose))
+(test-error (compose list))
+
+;; kestrels
+(test-equal 'x ((kestrels 'x) 'y))
+(test-equal 'x ((kestrels 'x) 'y0 'y1))
+(test-equal '(x0 x1)
+	    (let-values ((r ((kestrels 'x0 'x1) 'y0 'y1))) r))
+
+;; thrush
+(let ((add2 (pa$ + 2))
+      (times2 (pa$ * 2))
+      (id (lambda (x) x))
+      (value1->2 (lambda (x) (values x x))))
+  (test-equal 3 ((thrush 1) add2))
+  (test-equal 5 ((thrush 1 2) add2))
+  
+  (test-equal 6 ((thrush 1) add2 times2))
+  (test-equal 10 ((thrush 1 2) add2 times2))
+  (test-equal 5 ((thrush 1 2) add2 id))
+  (test-equal '(5 5) (let-values ((r ((thrush 1 2) add2 id value1->2))) r))
+  (test-error ((thrush 1 2) id add2))
+
+  (test-equal 10 (~> 3 add2 times2))
+  )
+
+;; cardinal
+(let ((add2 (pa$ + 2))
+      (id (lambda (f) f)))
+  (test-equal 5 (((cardinal id) 3) add2)))
+
+
+(let ((add3 (pa$ + 3)))
+  (test-equal 7 (add3 4))
+  (test-equal '(2 4 6) (map (pa$ * 2) '(1 2 3))))
+(let ((add (pa$ +)))
+  (test-equal 6 (add 1 2 3)))
+(let ((add3-4 (pa$ + 3 4)))
+  (test-equal 10 (add3-4 3)))
+
+(let ((apply-cons (apply$ cons)))
+  (test-assert (procedure? apply-cons))
+  (test-equal '(1 . 2) (apply-cons 1 '(2))))
+(let ((map2* (map$ (pa$ * 2))))
+  (test-assert (procedure? map2*))
+  (test-equal '(2 4 6) (map2* '(1 2 3))))
+(let ((for-each2$ (for-each$ (lambda (a) (display a)))))
+  (test-assert (procedure? for-each2$))
+  (test-equal "123" (with-output-to-string (lambda () (for-each2$ '(1 2 3))))))
+
+(test-equal '(#t #f #t) (map (complement even?) '(1 2 3)))
+(test-equal '(#f #t #f) (map (complement =) '(1 2 3) '(1 1 3)))
+(test-assert ((complement (lambda () #f))))
+
+(let ((string-or-symbol? (any-pred string? symbol?)))
+  (test-assert (string-or-symbol? "abc"))
+  (test-assert (string-or-symbol? 'abc))
+  (test-assert (not (string-or-symbol? 3))))
+
+(let ((<> (any-pred < >)))
+  (test-assert (<> 3 4))
+  (test-assert (not (<> 3 3))))
+
+(test-equal '(b c)
+	    ((any-pred (cut memq <> '(a b c)) (cut memq <> '(1 2 3))) 'b))
+
+(test-assert exists-pred)
+
+(test-assert ((every-pred odd? positive?) 3))
+(test-assert (not ((every-pred odd? positive?) 4)))
+(test-assert (not ((every-pred odd? positive?) -3)))
+
+(let ((safe-length (every-pred list? length)))
+  (test-equal 3 (safe-length '(a b c)))
+  (test-assert (not (safe-length "aaa"))))
+
+(test-end)
