13fa4a481048897d40f691ac343af0bc34dd89ed
ktakashi <ktakashi@ymail.com>
2017-11-23 14:31:57 +0100

    Fixing copyright year

--- sitelib/rfc/json-pointer.scm
+++ sitelib/rfc/json-pointer.scm
@@ -2,7 +2,7 @@
 ;;;
 ;;; rfc/json-pointer - JSON Pointer
 ;;;  
-;;;   Copyright (c) 2010-2015  Takashi Kato  <ktakashi@ymail.com>
+;;;   Copyright (c) 2017  Takashi Kato  <ktakashi@ymail.com>
 ;;;   
 ;;;   Redistribution and use in source and binary forms, with or without
 ;;;   modification, are permitted provided that the following conditions
