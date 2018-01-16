3189239417b717bf58aec7008645fc90df49a5f5
ktakashi <ktakashi@ymail.com>
2018-01-16 14:36:59 +0100

    fixing calendar test

--- ext/time/test-calendar.scm
+++ ext/time/test-calendar.scm
@@ -5,7 +5,7 @@
 (test-begin "calendar")
 
 (test-assert (calendar? calendar:gregorian))
-(test-assert (calendar? calendar:julian))
+(test-assert (calendar? calendar:iso))
 (test-assert (calendar? calendar:system))
 
 (test-assert (calendar-date? (time-utc->calendar-date (current-time))))
