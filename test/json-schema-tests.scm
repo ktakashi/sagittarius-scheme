c2b1b6eb6a8f01c818434e8fe084bbc4fc8c6e0c
Takashi Kato <ktakashi@ymail.com>
2018-08-27 13:22:09 +0200

    Adding simple HTTP server providing remote schema

--- test/json-schema-tests.scm
+++ test/json-schema-tests.scm
@@ -1,15 +1,42 @@
+#!read-macro=sagittarius/regex
 (import (rnrs)
 	(text json)
 	(text json schema)
 	(text json validator)
+	(util file)
+	(net server)
+	(sagittarius socket)
+	(sagittarius regex)
 	(util file)
 	(srfi :133)
 	(chibi test))
 
 ;; This test requires external git repository, so runs only on CI
 ;; environment using separated CMake command.
-
-;; TODO make http server with port 1234 to retrieve remote schema.
+(define remote-directory "JSON-Schema-Test-Suite/remotes")
+(define (http-handler server socket)
+  (define in/out (transcoded-port (socket-port socket) (native-transcoder)))
+  (define (get-content path)
+    (let ((file (if (string=? path "/folderInteger.json")
+		    (string-append remote-directory "/folder" path)
+		    (string-append remote-directory path))))
+      (and (file-exists? file)
+	   (file->string file))))
+  (let ((line (get-line in/out)))
+    (cond ((#/GET (.+?) HTTP/ line) =>
+	   (lambda (m)
+	     (cond ((get-content (m 1)) =>
+		    (lambda (content)
+		      (put-string in/out "HTTP/1.1 200 OK\r\n")
+		      (put-string in/out "Content-Length: ")
+		      (put-string in/out (number->string (string-length content)))
+		      (put-string in/out "\r\n")
+		      (put-string in/out "Content-Type: application/json\r\n\r\n")
+		      (put-string in/out content)))
+		   (else (put-string in/out "HTTP/1.1 404 Not Found\r\n\r\n")))))
+	  (else (put-string in/out "HTTP/1.1 404 Not Found\r\n\r\n")))))
+(define server (make-simple-server "1234" http-handler))
+(server-start! server :background #t)
 
 (define data-directory "JSON-Schema-Test-Suite/tests/draft7/")
 (define files
@@ -45,3 +72,5 @@
 (for-each run-schema-tests files)
 
 (test-end)
+
+(server-stop! server)
