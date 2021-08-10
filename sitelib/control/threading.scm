b0257ead24d6d3af3ae6766284c5ed262a207426
Takashi Kato <ktakashi@ymail.com>
2021-08-10 15:10:42 +0200

    Adding if~> and cond~>

--- sitelib/control/threading.scm
+++ sitelib/control/threading.scm
@@ -29,8 +29,8 @@
 ;;;  
 
 (library (control threading)
-    (export ~> lazy~>
-	    force ;; for convenience
+    (export ~> lazy~> if~> cond~> 
+	    => else force ;; for convenience
 	    )
     (import (rnrs)
 	    (scheme lazy))
@@ -50,4 +50,27 @@
        (lazy~> "thread" (delay-force (proc (force v))) exp* ...)))
     ((_ seed exp* ...)
      (lazy~> "thread" (delay-force seed) exp* ...))))
+
+(define-syntax if~>
+  (syntax-rules ()
+    ((_ seed pred then) (if~> seed pred then (lambda (v) #f)))
+    ((_ seed pred then else)
+     (let ((p pred) (t then) (e else))
+       (lazy~> seed (lambda (v) (if (p v) (t v) (e v))))))))
+
+(define-syntax cond~>
+  (syntax-rules (=> else)
+    ((_ "parse" seed ((pred exp* ...)))
+     (let ((t (lambda (v) (lazy~> v exp* ...))))
+       (if~> seed pred t)))
+    ((_ "parse" seed ((pred exp* ...) (else exp2* ...)))
+     (let ((t (lambda (v) (lazy~> v exp* ...)))
+	   (e (lambda (v) (lazy~> v exp2* ...))))
+       (if~> seed pred t e)))
+    ((_ "parse" seed ((pred exp* ...) clause* ...))
+     (let ((t (lambda (v) (lazy~> v exp* ...))))
+       (if~> seed pred t (cond~> "parse" seed (clause* ...)))))
+    ((_ seed clause* ...)
+     (cond~> "parse" seed (clause* ...)))))
+
 )
