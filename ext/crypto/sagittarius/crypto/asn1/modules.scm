315c390e3af830e1ab382dbc554cbd3d28c39ba4
Takashi Kato <ktakashi@ymail.com>
2022-10-24 21:34:31 +0200

    Adding sequence / set of

--- ext/crypto/sagittarius/crypto/asn1/modules.scm
+++ ext/crypto/sagittarius/crypto/asn1/modules.scm
@@ -33,7 +33,7 @@
 ;; encoder
 #!nounbound
 (library (sagittarius crypto asn1 modules)
-    (export define-asn1-encodable
+    (export define-asn1-encodable of
 	    asn1-object->asn1-encodable
 	    ;; For now only these two, maybe we want to add
 	    ;; asn1-choice and others as well
@@ -47,7 +47,7 @@
 	    (sagittarius crypto asn1 types))
 
 (define-generic asn1-object->asn1-encodable)
-
+(define-syntax of (syntax-rules ()))
 ;; we want to define ASN.1 module like this
 ;; e.g.
 ;; AlgorithmIdentifier
@@ -65,13 +65,35 @@
 ;;     (request-list :type (sequence-of <request>))
 ;;     (request-extensions :type <extensions> :tag 2 :optional #t))))
 (define-syntax define-asn1-encodable
-  (syntax-rules ()
+  (syntax-rules (of)
+    ((_ name (base-type (of spec ...) opts ...))
+     (begin
+       (define-asn1-encodable "emit-class" name
+	 (base-type ((of :multiple #t
+			 spec ...
+			 :init-keyword :elements ;; default
+			 :init-value '())) opts ...))
+       (define-method write-object ((o name) p)
+	 (asn1-generic-write name
+			     (asn1-object-list->string (slot-ref o 'of)) p))))
     ((_ name (base-type ((slot spec* ...) ...) opts ...))
      (begin
+       (define-asn1-encodable "emit-class" name
+	 (base-type ((slot spec* ...) ...) opts ...))
+       (define-method write-object ((o name) p)
+	 (define (slots->list o)
+	   (filter-map (lambda (s) (slot-ref o (slot-definition-name s)))
+		       (class-slots name)))
+	 (asn1-generic-write name
+			     (asn1-object-list->string (slots->list o)) p))))
+    ((_ "emit-class" name (base-type ((slot spec* ...) ...) opts ...))
+     (begin
        (define-class name (<asn1-encodable>)
-	 ((slot :init-keyword (symbol->keyword 'slot)
-		:init-value #f ;; for optional
-		spec* ...) ...))
+	 ((slot spec* ...
+		;; needs to be after
+		:init-keyword (symbol->keyword 'slot)
+		;; for optional
+		:init-value #f) ...))
        (define asn1-object->this
 	 (make-asn1-object->asn1-encodable name (base-type predicate)))
        (define this->asn1-object
@@ -81,13 +103,10 @@
 						   (o <asn1-object>))
 	 (asn1-object->this o))
        (define-method asn1-encodable->asn1-object ((o name) type)
-	 (this->asn1-object o type))
-       (define-method write-object ((o name) p)
-	 (define (slots->list o)
-	   (filter-map (lambda (s) (slot-ref o (slot-definition-name s)))
-		       (class-slots name)))
-	 (asn1-generic-write name
-			     (asn1-object-list->string (slots->list o)) p))))))
+	 (this->asn1-object o type))))))
+
+;; <asn1-object> is always <asn1-encodable>, so this is fine
+(define-method asn1-object->asn1-encodable (m (o <asn1-object>)) o)
 
 ;; dispatcher
 (define-syntax predicate (syntax-rules ()))
@@ -99,27 +118,36 @@
     ((_ constructor opts ...)
      (lambda (type) (if (eq? type 'der) make-der-sequence make-ber-sequence)))))
 (define-syntax asn1-set
-  (syntax-rules (predicate constructor)))
+  (syntax-rules (predicate constructor)
+    ((_ predicate) ber-set?)
+    ((_ constructor opts ...)
+     (lambda (type)
+       (lambda (lis)
+	 (if (eq? type 'der)
+	     (make-der-set lis opts ...)
+	     (make-ber-set lis opts ...)))))))
 
 ;; Internal APIs
 (define ((make-asn1-object->asn1-encodable class type?) (o type?))
   (define (asn1-object->slots class o slots)
-    (define (err)
+    (define (err reason)
       (error 'asn1-object->asn1-encodable
-	     (format "Given ASN.1 object can't be converted to an object of ~a"
-		     (class-name class))
+	     (format
+	      "Given ASN.1 object can't be converted to an object of ~a: ~a"
+	      (class-name class)
+	      reason)
 	     o))
     (define (entry->slot slot entry)
       (list (symbol->keyword (slot-definition-name slot)) entry))
     (define (optional? slot) (slot-definition-option slot :optional #f))
-    (define (try-deserialize type entry)
-      (guard (e (else #f))
+    (define (try-deserialize type entry optional?)
+      (guard (e (else (if optional? #f (raise e))))
 	(asn1-object->asn1-encodable type entry)))
-    (define (entry->object entry type)
+    (define (entry->object entry type optional?)
       (cond ((is-a? entry type) entry)
 	    ((and (asn1-collection? entry)
 		  (subtype? type <asn1-encodable>)
-		  (try-deserialize type entry)))
+		  (try-deserialize type entry optional?)))
 	    (else #f)))
     (let loop ((entries (asn1-collection->list o)) (slots slots) (r '()))
       (cond ((and (null? entries) (null? slots))
@@ -128,8 +156,9 @@
 	    ((null? entries)
 	     (if (or (null? slots) (for-all optional? slots))
 		 (concatenate! r)
-		 (err)))
-	    ((null? slots) (if (null? entries) (concatenate! r) (err)))
+		 (err "missing field(s)")))
+	    ((null? slots)
+	     (if (null? entries) (concatenate! r) (err "too many fields")))
 	    (else
 	     (let* ((slot (car slots))
 		    (entry (car entries))
@@ -141,20 +170,29 @@
 		      (cond ((and (ber-tagged-object? entry)
 				  (= (ber-tagged-object-tag-no entry) tag))
 			     (let ((obj (ber-tagged-object-obj entry)))
-			       (cond ((entry->object obj type) =>
+			       (cond ((entry->object obj type optional?) =>
 				      (lambda (obj)
 					(loop (cdr entries) (cdr slots)
 					      (cons (entry->slot slot obj) r))))
-				     (else (err)))))
+				     (else (err "Invalid tag object")))))
 			    (optional? (loop entries (cdr slots) r))
-			    (else (err))))
-		     ((entry->object entry type) =>
+			    (else (err "Missing tag object"))))
+		     ((entry->object entry type optional?) =>
 		      (lambda (obj)
 			(loop (cdr entries) (cdr slots)
 			      (cons (entry->slot slot obj) r))))
 		     (optional? (loop entries (cdr slots) r))
-		     (else (err))))))))
-  (apply make class (asn1-object->slots class o (class-slots class))))
+		     (else (err "Unknown field"))))))))
+  (let ((slots (class-slots class)))
+    (if (and (null? (cdr slots))
+	     (slot-definition-option (car slots) :multiple #f))
+	(let ((key (slot-definition-option (car slots)
+					   :init-keyword :elements))
+	      (type (slot-definition-option (car slots) :type <asn1-object>)))
+	  (make class
+	    key (map (lambda (o) (asn1-object->asn1-encodable type o))
+		     (asn1-collection->list o))))
+	(apply make class (asn1-object->slots class o slots)))))
 
 
 (define ((make-asn1-encodable->asn1-object class make-ctr) o type)
@@ -163,19 +201,24 @@
       (cond ((asn1-object? o) o)
 	    ((asn1-encodable? o) (asn1-encodable->asn1-object o type))
 	    (else o)))
-    (filter-map (lambda (s)
-		  (let ((tag (slot-definition-option s :tag #f))
-			(explicit? (slot-definition-option s :explicit #f))
-			(obj (ensure-asn1-object
-			      (slot-ref o (slot-definition-name s)))))
-		    (and obj
-			 (if tag
-			     (make (if (eq? type 'der)
-				       <der-tagged-object>
-				       <ber-tagged-object>)
-			       :tag-no tag :explicit? explicit? :obj obj)
-			     obj))))
-		(class-slots (class-of o))))
+    (let ((slots (class-slots (class-of o))))
+      (if (and (null? (cdr slots))
+	       (slot-definition-option (car slots) :multiple #f))
+	  (map ensure-asn1-object (slot-ref o 'of))
+	  (filter-map (lambda (s)
+			(let ((tag (slot-definition-option s :tag #f))
+			      (explicit?
+			       (slot-definition-option s :explicit #f))
+			      (obj (ensure-asn1-object
+				    (slot-ref o (slot-definition-name s)))))
+			  (and obj
+			       (if tag
+				   (make (if (eq? type 'der)
+					     <der-tagged-object>
+					     <ber-tagged-object>)
+				     :tag-no tag :explicit? explicit? :obj obj)
+				   obj))))
+		      slots))))
   ((make-ctr type) (object->asn1-object o)))
   
 )
