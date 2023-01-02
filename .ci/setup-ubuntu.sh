7af0ddaca0c8001cc307536667c3ddb98e8c2b5e
Takashi Kato <ktakashi@ymail.com>
2023-01-02 13:52:50 +0100

    Adding sudo to GitHub CI

--- .ci/setup-ubuntu.sh
+++ .ci/setup-ubuntu.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-apt update
-apt install --no-install-recommends --no-install-suggests -y \
-    libgc-dev zlib1g-dev libffi-dev libffi-dev libssl-dev \
-    cmake make curl gcc g++
+SUDO=$1
+
+$SUDO apt update
+$SUDO apt install --no-install-recommends --no-install-suggests -y \
+      libgc-dev zlib1g-dev libffi-dev libffi-dev libssl-dev \
+      cmake make curl gcc g++
 
