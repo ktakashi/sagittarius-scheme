828c8122d6b24f171d4d8b0c3008edf027ec7d70
Takashi Kato <ktakashi@ymail.com>
2023-09-06 13:44:33 +0200

    Adding (security password) library

---
+++ sitelib/security/password.scm
@@ -0,0 +1,63 @@
+;;; -*- mode:scheme; coding:utf-8; -*-
+;;;
+;;; security/password.scm - Password policy
+;;;
+;;;   Copyright (c) 2023  Takashi Kato  <ktakashi@ymail.com>
+;;;
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;
+
+#!nounbound
+(library (security password)
+    (export password-policy? password-policies
+	    password-policy-compliant?
+
+	    length-policy? make-length-policy
+	    character-policy? make-character-policy
+	    ;; predefined policy creators
+	    make-lower-case-policy
+	    make-upper-case-policy
+	    make-symbol-policy
+	    make-digit-policy)
+    (import (rnrs)
+	    (security password policy)
+	    (security password checker)
+	    (srfi :14 char-sets))
+
+(define (->ascii-char-set cs) (char-set-intersection char-set:ascii cs))
+(define char-set:lower-case/ascii (->ascii-char-set char-set:lower-case))
+(define char-set:upper-case/ascii (->ascii-char-set char-set:upper-case))
+(define char-set:symbol/ascii (->ascii-char-set char-set:symbol))
+(define char-set:digit/ascii (->ascii-char-set char-set:digit))
+
+(define (make-lower-case-policy at-least)
+  (make-character-policy char-set:lower-case/ascii at-least))
+(define (make-upper-case-policy at-least)
+  (make-character-policy char-set:upper-case/ascii at-least))
+(define (make-symbol-policy at-least)
+  (make-character-policy char-set:symbol/ascii at-least))
+(define (make-digit-policy at-least)
+  (make-character-policy char-set:digit/ascii at-least))
+
+)
