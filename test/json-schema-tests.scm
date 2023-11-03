51b23e55ee481e90f74a70cc987d7dc593a30c89
Takashi Kato <ktakashi@ymail.com>
2023-11-03 14:56:07 +0100

    Enabling Draft 2019-09 tests

--- test/json-schema-tests.scm
+++ test/json-schema-tests.scm
@@ -41,7 +41,7 @@
 
 (define data-directories
   '("JSON-Schema-Test-Suite/tests/draft7/"
-    #;"JSON-Schema-Test-Suite/tests/draft2019-09/"))
+    "JSON-Schema-Test-Suite/tests/draft2019-09/"))
 (define core-files
   (append-map (lambda (dir) (find-files dir :recursive #f :pattern "\\.json$"))
 	      data-directories))
@@ -53,7 +53,14 @@
 
 ;; the *will not be supported/fixed* test names
 (define *known-incompatible-tests* '())
-(define *ignoring-scenarios* '())
+(define *ignoring-scenarios*
+  '())
+
+(define *ignoring-files*
+  '(
+    ;; We don't support meta schema, too much hastle
+    "JSON-Schema-Test-Suite/tests/draft2019-09/vocabulary.json"
+    ))
 
 ;; FIXME copy&paste...
 (define (key=? key) (lambda (e) (and (pair? e) (string=? (car e) key) e)))
@@ -79,8 +86,9 @@
       (test-group description
 	(let ((validator (json-schema->json-validator schema)))
 	  (for-each (lambda (test) (run-validator validator test)) tests)))))
-  (test-group file
-    (for-each run-schema-test (call-with-input-file file json-read))))
+  (unless (member file *ignoring-files*)
+    (test-group file
+      (for-each run-schema-test (call-with-input-file file json-read)))))
 
 (test-begin "JSON Schema test")
 
@@ -93,7 +101,11 @@
   )
 (server-stop! server)
 
+(newline)
 (display "##############################################") (newline)
+(display "These test files are skipped") (newline)
+(for-each (lambda (test) (display "- ") (display test) (newline))
+	  *ignoring-files*)
 (display "These test scenarios are skipped") (newline)
 (for-each (lambda (test) (display "- ") (display test) (newline))
 	  *ignoring-scenarios*)
