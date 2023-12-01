01a608b0e4caa1409d072a6d975fe6f499130721
Takashi Kato <ktakashi@ymail.com>
2023-12-01 09:59:09 +0100

    Adding Windows CI on github actions

---
+++ .ci/install-current.ps1
@@ -0,0 +1,10 @@
+$net = new-object net.webclient
+
+# seems we can't rely on where this is executed
+md -Force \tmp
+# install the latest Sagittarius
+$url = 'https://bitbucket.org/ktakashi/sagittarius-scheme/downloads'
+$net.DownloadFile("$($url)/latest-version.txt", '\tmp\latest-version.txt')
+$version=(Get-Content \tmp\latest-version.txt)
+$net.DownloadFile("$($url)/setup_sagittarius_$($version)_x64.exe", '\tmp\setup_sagittarius.exe')
+\tmp\setup_sagittarius.exe /VERYSILENT /SUPPRESSMSGBOXES
