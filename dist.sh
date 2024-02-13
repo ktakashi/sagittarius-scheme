4e1c105ae130b42d0a54c66c35cd88b56d814cb5
Takashi Kato <ktakashi@ymail.com>
2024-02-13 13:35:35 +0100

    Removing git and CI related files from dist archive

--- dist.sh
+++ dist.sh
@@ -134,9 +134,11 @@ dist() {
     
 # copy to tmp directory
     mkdir ${DIST_NAME}
-# use rsync to exclude .hg stuff
+# use rsync to exclude hidden or CI files
 # I hope this command exists any where...
-    rsync -r --exclude='.hg*' --exclude="bitbucket-pipelines.yml" ${SRC_DIR}/* ${DIST_NAME}
+    rsync -r --exclude='.hg*' --exclude="bitbucket-pipelines.yml" \
+	  --exclude='*.git*' --exclude='.ci*' \
+	  ${SRC_DIR}/* ${DIST_NAME}
     
     tar -czvf ${DIST_NAME}.tar.gz ${DIST_NAME}
     
