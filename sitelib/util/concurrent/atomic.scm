cc38351fb8cf3d25867e1db36147fc8fe2c898e8
Takashi Kato <ktakashi@ymail.com>
2025-02-03 12:00:51 +0100

    Adding lock-free-queue

--- sitelib/util/concurrent/atomic.scm
+++ sitelib/util/concurrent/atomic.scm
@@ -58,5 +58,98 @@
 	    atomic-thread-fence
 
 	    atomic-fixnum-inc!
-	    atomic-fixnum-dec!)
-    (import (sagittarius atomic)))
+	    atomic-fixnum-dec!
+
+	    lock-free-queue? make-lock-free-queue
+	    list->lock-free-queue lock-free-queue
+	    lock-free-queue-size lock-free-queue-empty?
+
+	    lock-free-queue-push!
+	    lock-free-queue-pop!
+	    lock-free-queue-get!
+
+	    lock-free-queue->list)
+    (import (rnrs)
+	    (sagittarius atomic)
+	    (srfi :1 lists))
+
+;; lock-free queue
+;; ref
+;; - Even Better DCAS-Based Concurrent Deques
+;;   DavidL.Detlefs, ChristineH.Flood, AlexanderT.Garthwaite, Paul A. Martin,
+;;   Nir N. Shavit, and Guy L. Steele Jr.
+;; - DCASis not a Silver Bullet for Nonblocking Algorithm
+;;   Simon Doherty, Victor Luchangco, David L. Detlefs, Paul A. Martin,
+;;   Lindsay Groves, Mark Moir, Christine H. Flood, Nir Shavit,
+;;   Guy L. Steele Jr.
+;;
+;; Above papers discuss deque however we do linked list using pair.
+;; We support the below operations
+;; - push (= pushRight)
+;; - pop  (= popRight)
+;; - get  (= popLeft)
+;; atomic-pair = { head, tail } = { LeftHat, RightHat }
+(define-record-type lock-free-queue 
+  (fields atomic-pair atomic-size)
+  (protocol
+   (lambda (p)
+     (case-lambda
+      (() (p (make-atomic-pair '() '()) (make-atomic-fixnum 0)))
+      ((lis) (p (make-atomic-pair p (cdr p))
+		(make-atomic-fixnum (length lis))))))))
+(define *empty-value* (cons '() '()))
+(define (empty-value? e) (equal? *empty-value* e))
+
+(define (list->lock-free-queue lis) (make-lock-free-queue lis))
+(define (lock-free-queue . lis) (list->lock-free-queue lis))
+(define (lock-free-queue-size q)
+  (atomic-fixnum-load (lock-free-queue-atomic-size q)))
+
+(define (lock-free-queue->list q) 
+  (let ((r (atomic-load (lock-free-queue-atomic-pair q))))
+    (if (empty-value? r)
+	'()
+	r)))
+(define (lock-free-queue-empty? q) (zero? (lock-free-queue-size q)))
+
+(define (lock-free-queue-push! q e)
+  (define atomic-pair (lock-free-queue-atomic-pair q))
+  (define atomic-fx (lock-free-queue-atomic-size q))
+  (define (try-push ap expected element)
+    (define new-val (if (lock-free-queue-empty? q) 
+			(list element)
+			`(,@expected ,element)))
+    (cond ((atomic-compare-and-swap! ap expected new-val)
+	   (atomic-fixnum-inc! atomic-fx)
+	   e)
+	  (else (try-push ap (atomic-load ap) element))))
+  (try-push atomic-pair (atomic-load atomic-pair) e))
+
+(define (lock-free-queue-pop! q)
+  (define atomic-pair (lock-free-queue-atomic-pair q))
+  (define atomic-fx (lock-free-queue-atomic-size q))
+  (define (try-get ap expected)
+    (define new-val
+      (if (null? (cdr expected))
+	  *empty-value*
+	  (cons (car expected) (drop-right (cdr expected) 1))))
+    (cond ((atomic-compare-and-swap! ap expected new-val)
+	   (atomic-fixnum-dec! atomic-fx)
+	   (car (take-right expected 1)))
+	  (else (try-get ap (atomic-load ap)))))
+  (try-get atomic-pair (atomic-load atomic-pair)))
+
+(define (lock-free-queue-get! q)
+  (define atomic-pair (lock-free-queue-atomic-pair q))
+  (define atomic-fx (lock-free-queue-atomic-size q))
+  (define (try-get ap expected)
+    (define new-val (if (null? (cdr expected))
+			*empty-value*
+			(cdr expected)))
+    (cond ((atomic-compare-and-swap! ap expected new-val)
+	   (atomic-fixnum-dec! atomic-fx)
+	   (car expected))
+	  (else (try-get ap (atomic-load ap)))))
+  (try-get atomic-pair (atomic-load atomic-pair)))
+
+)
