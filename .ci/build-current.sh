d29590b4bd85568bcbba02589b2dca03a285951f
Takashi Kato <ktakashi@ymail.com>
2025-06-18 15:13:38 +0200

    Using GitHub release on CI

--- .ci/build-current.sh
+++ .ci/build-current.sh
@@ -2,13 +2,20 @@
 
 NEED_SUDO=$1
 
-REPOSITORY_URL=https://bitbucket.org/ktakashi/sagittarius-scheme/downloads
+REPOSITORY_URL=https://github.com/ktakashi/sagittarius-scheme
+RELEASES=${REPOSITORY_URL}/releases
+# FIXME: ugly...
+VERSION=$(curl -sI "${RELEASES}/latest" \
+	| grep -i '^Location:' \
+	| tr '\r\n' ' ' \
+	| sed "s|Location: ${RELEASES}/tag/v||i" \
+        | sed 's/ //g')
 
-curl -kLo version ${REPOSITORY_URL}/latest-version.txt
-curl -kLo sagittarius.tar.gz ${REPOSITORY_URL}/sagittarius-`cat version`.tar.gz
+DOWNLOAD="${RELEASES}/download/v${VERSION}/sagittarius-${VERSION}.tar.gz"
+curl -kLo sagittarius.tar.gz $DOWNLOAD
 
 tar --no-same-owner -xvf sagittarius.tar.gz
-cd sagittarius-`cat version`
+cd "sagittarius-${VERSION}"
 
 cmake .
 make -j8 sash
--- .ci/install-current.ps1
+++ .ci/install-current.ps1
@@ -2,9 +2,13 @@ $net = new-object net.webclient
 
 # seems we can't rely on where this is executed
 md -Force \tmp
+
 # install the latest Sagittarius
-$url = 'https://bitbucket.org/ktakashi/sagittarius-scheme/downloads'
-$net.DownloadFile("$($url)/latest-version.txt", '\tmp\latest-version.txt')
-$version=(Get-Content \tmp\latest-version.txt)
-$net.DownloadFile("$($url)/setup_sagittarius_$($version)_x64.exe", '\tmp\setup_sagittarius.exe')
+$url = 'https://github.com/ktakashi/sagittarius-scheme/releases'
+$response = Invoke-WebRequest -Uri "$url/latest" -MaximumRedirection 0 -ErrorAction Ignore
+$response.Headers.Location -match "$url/tag/v(?<version>.+)"
+$version = $matches['version']
+$download_url = "$url/download/v$version/setup_sagittarius_$($version)_x64.exe"
+$net.DownloadFile($download_url, '\tmp\setup_sagittarius.exe')
+
 Start-Process -FilePath '\tmp\setup_sagittarius.exe' -ArgumentList '/VERYSILENT', '/SUPPRESSMSGBOXES', '/ALLUSERS', '/LOG="\tmp\setup.log"' -Wait
