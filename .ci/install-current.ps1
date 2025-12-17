9e576e32776fc80f8c3f6b3e55ad0e7b5241f8dd
Takashi Kato <ktakashi@ymail.com>
2025-12-17 21:27:19 +0100

    Investigation for GitHub Actions failure...

--- .ci/install-current.ps1
+++ .ci/install-current.ps1
@@ -3,9 +3,12 @@ $net = new-object net.webclient
 # seems we can't rely on where this is executed
 md -Force \tmp
 
+Write-Output $PSVersionTable.PSVersion
+
 # install the latest Sagittarius
 $url = 'https://github.com/ktakashi/sagittarius-scheme/releases'
-$response = Invoke-WebRequest -Uri "$url/latest" -MaximumRedirection 0 -ErrorAction Ignore
+Write-Output "$url/latest"
+$response = Invoke-WebRequest -Uri "$url/latest" -UseBasicParsing -MaximumRedirection 0 -ErrorAction Ignore
 $response.Headers.Location -match "$url/tag/v(?<version>.+)"
 $version = $matches['version']
 $download_url = "$url/download/v$version/setup_sagittarius_$($version)_x64.exe"
