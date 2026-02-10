18b6a01b21d4bcc747b51c3ebe47581de0035fb9
Takashi Kato <ktakashi@ymail.com>
2026-02-10 09:09:01 +0100

    Adding call/cc compat tests

--- test/tests/sagittarius/continuations.scm
+++ test/tests/sagittarius/continuations.scm
@@ -38,4 +38,72 @@
        (lambda (x) 'outer x))))
   (test-equal 'invoked (check)))
 
+(define-syntax test
+  (lambda (x)
+    (syntax-case x ()
+      ((k expr expected)
+       (with-syntax ((call/cc (datum->syntax #'k 'call/cc))
+		     (call-with-current-continuation
+		      (datum->syntax #'k 'call-with-current-continuation)))
+	 #'(begin
+	     (define (a-test call/cc call-with-current-continuation)
+	       (call-with-continuation-prompt
+		(lambda () (test-equal expected expr))))
+	     ;; should work the same
+	     (a-test call/cc call-with-current-continuation)
+	     (a-test call/delim-cc
+		     call-with-delimited-current-continuation)))))))
+
+;; From R6RS test
+(test (let ((path '())
+            (c #f))
+        (let ((add (lambda (s)
+                     (set! path (cons s path)))))
+          (dynamic-wind
+              (lambda () (add 'connect))
+              (lambda ()
+                (add (call-with-current-continuation
+                      (lambda (c0)
+                        (set! c c0)
+                        'talk1))))
+              (lambda () (add 'disconnect)))
+          (if (< (length path) 4)
+              (c 'talk2)
+              (reverse path))))
+      '(connect talk1 disconnect
+                connect talk2 disconnect))
+
+(test (let ((n 0))
+        (call-with-current-continuation
+         (lambda (k)
+           (dynamic-wind
+               (lambda ()
+                 (set! n (+ n 1))
+                 (k))
+               (lambda ()
+                 (set! n (+ n 2)))
+               (lambda ()
+                 (set! n (+ n 4))))))
+        n) 
+      1)
+
+(test (let ((n 0))
+        (call-with-current-continuation
+         (lambda (k)
+           (dynamic-wind
+               values
+               (lambda ()
+                 (dynamic-wind
+                     values
+                     (lambda ()
+                       (set! n (+ n 1))
+                       (k))
+                     (lambda ()
+                       (set! n (+ n 2))
+                       (k))))
+               (lambda ()
+                 (set! n (+ n 4))))))
+        n) 
+      7)
+
 (test-end)
